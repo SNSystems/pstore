58d8d58fdcfcb8f15b3da63ce6beabbde4d8a69f
Ying Yi <ying.yi@sony.com>
2017-10-31 12:41:41 +0000

    Removed the check whether atomic library exists in the check_atomic module.

--- CMakeModules/check_atomic.cmake
+++ CMakeModules/check_atomic.cmake
@@ -3,8 +3,8 @@ INCLUDE(CheckCXXSourceCompiles)
 # Sometimes linking against libatomic is required for atomic ops, if
 # the platform doesn't support lock-free atomics.
 
-# We could modify LLVM's CheckAtomic module and have it check for 64-bit
-# atomics instead.
+# We modified LLVM's CheckAtomic module and have it check for 64-bit
+# atomics.
 
 function(pstore_check_cxx_atomics varname)
   set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
@@ -12,10 +12,10 @@ function(pstore_check_cxx_atomics varname)
   check_cxx_source_compiles("
 #include <cstdint>
 #include <atomic>
-std::atomic<uint64_t> x (0);
+std::atomic<std::uint64_t> x (0);
 
 int main() {
-  return x.is_lock_free() ? 0 : 1;
+  return x.is_lock_free();
 }
 " ${varname})
   set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
@@ -23,17 +23,12 @@ endfunction(pstore_check_cxx_atomics)
 
 pstore_check_cxx_atomics(PSTORE_HAVE_CXX_ATOMICS_WITHOUT_LIB)
 
-# If not, check if the library exists, and atomics work with it.
+# If not, check if the atomic library works with it.
 if(NOT PSTORE_HAVE_CXX_ATOMICS_WITHOUT_LIB)
-  check_library_exists(atomic __atomic_fetch_add_8 "" PSTORE_HAS_ATOMIC_LIB)
-  if(PSTORE_HAS_ATOMIC_LIB)
-    list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
-    pstore_check_cxx_atomics(PSTORE_HAVE_CXX_ATOMICS_WITH_LIB)
-    if (NOT PSTORE_HAVE_CXX_ATOMICS_WITH_LIB)
-      message(WARNING "Host compiler must support std::atomic!")
-    endif()
-  else()
-    message(WARNING "Host compiler appears to require libatomic, but cannot find it.")
+  list(APPEND CMAKE_REQUIRED_LIBRARIES "atomic")
+  pstore_check_cxx_atomics(PSTORE_HAVE_CXX_ATOMICS_WITH_LIB)
+  if (NOT PSTORE_HAVE_CXX_ATOMICS_WITH_LIB)
+    message(WARNING "Host compiler must support std::atomic!")
   endif()
 endif()
 
