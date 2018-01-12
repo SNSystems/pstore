SNSystems/pstore

commit 97bfb838d3c55b967191a18aa90f1cd2f7a08562

Work for <https://github.com/SNSystems/pstore/issues/11>. Extend pstore::uuid to enable testing for a valid UUID string. This is needed because the user can specify a UUID string or a path as the argument --ticket. If want to try the UUID version first: if not valid then we trying looking for a file with the specified name.

lib/pstore/uuid.cpp
@@ -139,8 +139,6 @@ namespace pstore {
         assert (this->version () == uuid::version_type::random_number_based);
     }
 
-
-
 #ifdef _WIN32
     uuid::uuid (UUID const & u) {
         // UUID octets are in network byte order, but the Microsoft API
@@ -175,10 +173,22 @@ namespace pstore {
 
     // Construct from the canonical UUID string representation as defined by RFC4122.
     uuid::uuid (std::string const & str) {
-        if (str.length () != this->string_length) {
+        maybe <uuid> d = uuid::from_string (str);
+        if (!d.has_value()) {
             raise (pstore::error_code::uuid_parse_error);
         }
-        auto out = make_hex_decoder (std::begin (data_));
+        data_ = d->array ();
+    }
+
+    // from_string
+    // ~~~~~~~~~~~
+    maybe<uuid> uuid::from_string (std::string const & str) {
+        if (str.length () != string_length) {
+            return nothing <uuid> ();
+        }
+
+        container_type data;
+        auto out = make_hex_decoder (std::begin (data));
         auto count = 0U;
         for (auto digit : str) {
             switch (count++) {
@@ -187,7 +197,7 @@ namespace pstore {
             case 18:
             case 23:
                 if (digit != '-') {
-                    raise (pstore::error_code::uuid_parse_error);
+                    return nothing <uuid> ();
                 }
                 assert (out.is_high ());
                 break;
@@ -196,6 +206,8 @@ namespace pstore {
                 break;
             }
         }
+
+        return just (uuid {data});
     }
 
     // variant

include/pstore/uuid.hpp
@@ -53,6 +53,7 @@
 #include <cstdlib> // abort
 #include <string>
 
+#include "pstore_support/maybe.hpp"
 #include "pstore_support/portab.hpp"
 
 #if defined(_WIN32)
@@ -93,12 +94,17 @@ namespace pstore {
 
     public:
         uuid ();
-        /// Converts a string following the convention defined by RFC4122 to a UUID.
+        /// Converts a string following the convention defined by RFC4122 to a UUID. If the string is not valid an error is raised.
         explicit uuid (std::string const & s);
 
         /// A constructor used to construct a specific UUID from its binary value.
         uuid (container_type c)
                 : data_ (std::move (c)) {}
+
+        /// Converts a string to a UUID following the convention defined by RFC4122. If the string is not valid, returns nothing<uuid>.
+        /// \param s  A string to be converted to a UUID.
+        /// \returns  just<uuid>() if the string \p s was valid according to the description in RFC4122. If the string was invalid, nothing<uuid>.
+        static maybe<uuid> from_string (std::string const & s);
 
 #if defined(_WIN32)
         /// Convert from the native Win32 UUID type.
@@ -147,7 +153,7 @@ namespace pstore {
         std::string str () const;
 
     private:
-        std::array<std::uint8_t, elements> data_{};
+        container_type data_{};
 
         enum {
             version_octet = 6,

unittests/pstore/test_uuid.cpp
@@ -80,52 +80,58 @@ TEST_F (BasicUUID, Out) {
 }
 
 TEST (UUID, ParseStringBadLength) {
-    check_for_error ([]() { pstore::uuid _ ("00000000-0000-0000-0000-00000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("00000000-0000-0000-0000-0000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    {
+        std::string const input1 = "00000000-0000-0000-0000-00000000000000";
+        pstore::maybe <pstore::uuid> v1 = pstore::uuid::from_string (input1);
+        EXPECT_FALSE (v1.has_value ());
+
+        check_for_error ([&input1]() { pstore::uuid _ (input1); }, pstore::error_code::uuid_parse_error);
+    }
+    {
+        std::string const input2 = "00000000-0000-0000-0000-0000000000";
+        pstore::maybe <pstore::uuid> v2 = pstore::uuid::from_string (input2);
+        EXPECT_FALSE (v2.has_value ());
+
+        check_for_error ([&input2]() { pstore::uuid _ (input2); }, pstore::error_code::uuid_parse_error);
+    }
 }
 
 TEST (UUID, MissingDash) {
-    check_for_error ([]() { pstore::uuid _ ("0000000000000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("00000000-000000000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("00000000-0000-000000000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("00000000-0000-0000-00000000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string ("0000000000000-0000-0000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("00000000-000000000-0000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("00000000-0000-000000000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("00000000-0000-0000-00000000000000000").has_value ());
 }
 
 TEST (UUID, GoodHex) {
-    pstore::uuid expected{
-        pstore::uuid::container_type{{0x00, 0x99, 0xaa, 0xff, 0xAA, 0xFF, 0x09, 0x90, 0x00, 0x99,
-                                      0xaa, 0xff, 0xAA, 0xFF, 0x09, 0x90}}};
-    EXPECT_EQ (expected, pstore::uuid ("0099aaff-AAFF-0990-0099-aaffAAFF0990"));
+    std::string const input = "0099aaff-AAFF-0990-0099-aaffAAFF0990";
+    pstore::uuid const expected{
+        pstore::uuid::container_type{{0x00, 0x99, 0xAA, 0xFF, 0xAA, 0xFF, 0x09, 0x90, 0x00, 0x99,
+                                      0xAA, 0xFF, 0xAA, 0xFF, 0x09, 0x90}}};
+
+    // first try from_string()...
+    pstore::maybe<pstore::uuid> v = pstore::uuid::from_string (input);
+    ASSERT_TRUE (v.has_value ());
+    EXPECT_EQ (*v, expected);
+
+    // now using the constructor
+    EXPECT_EQ (expected, pstore::uuid (input));
 }
 
 TEST (UUID, BadHex) {
     // '/' is one behind '0' in ASCII
-    check_for_error ([]() { pstore::uuid _ ("/0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string ("/0000000-0000-0000-0000-000000000000").has_value ());
     // ':' is one past '9' in ASCII
-    check_for_error ([]() { pstore::uuid _ (":0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string (":0000000-0000-0000-0000-000000000000").has_value ());
     // '@' is one behind 'A' in ASCII
-    check_for_error ([]() { pstore::uuid _ ("@0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string ("@0000000-0000-0000-0000-000000000000").has_value ());
     // 'G' is one past 'F'. Too obvious?
-    check_for_error ([]() { pstore::uuid _ ("G0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("0G000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string ("G0000000-0000-0000-0000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("0G000000-0000-0000-0000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("g0000000-0000-0000-0000-000000000000").has_value ());
+    EXPECT_FALSE (pstore::uuid::from_string ("0g000000-0000-0000-0000-000000000000").has_value ());
     // '`' is one behind 'a' in ASCII
-    check_for_error ([]() { pstore::uuid _ ("`0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("g0000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
-    check_for_error ([]() { pstore::uuid _ ("0g000000-0000-0000-0000-000000000000"); },
-                     pstore::error_code::uuid_parse_error);
+    EXPECT_FALSE (pstore::uuid::from_string ("`0000000-0000-0000-0000-000000000000").has_value ());
 }
 
 TEST (UUID, MixedCase) {

commit 8073a0c7df34fac56fcea59c96dd0eecd5fb1dc1

Fix for commit 97bfb838d3c55b967191a18aa90f1cd2f7a08562 broke the pstore unit tests. Unfortunately, I didn't notice that the hex_to_digit() function raises an error if it see a bad input digit. This change moves the hex_to_digit() code into uuid::from_string() to that it can return nothing<>() directly.

lib/pstore/uuid.cpp
@@ -64,19 +64,6 @@
 #endif
 
 namespace {
-    unsigned hex_to_digit (char digit) {
-        if (digit >= 'a' && digit <= 'f') {
-            return static_cast<unsigned> (digit) - ('a' - 10);
-        }
-        if (digit >= 'A' && digit <= 'F') {
-            return static_cast<unsigned> (digit) - ('A' - 10);
-        }
-        if (digit >= '0' && digit <= '9') {
-            return static_cast<unsigned> (digit) - '0';
-        }
-        raise (pstore::error_code::uuid_parse_error);
-    }
-
     char digit_to_hex (unsigned v) {
         assert (v < 0x10);
         return static_cast<char> (v + ((v < 10) ? '0' : 'a' - 10));
@@ -202,7 +189,15 @@ namespace pstore {
                 assert (out.is_high ());
                 break;
             default:
-                out.append (hex_to_digit (digit));
+                if (digit >= 'a' && digit <= 'f') {
+                    out.append (static_cast<unsigned> (digit) - ('a' - 10));
+                } else if (digit >= 'A' && digit <= 'F') {
+                    out.append (static_cast<unsigned> (digit) - ('A' - 10));
+                } else if (digit >= '0' && digit <= '9') {
+                    out.append (static_cast<unsigned> (digit) - '0');
+                } else {
+                    return nothing <uuid> ();
+                }
                 break;
             }
         }