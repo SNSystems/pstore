SNSystems/pstore

commit 73a22b93904d57614604604a07c1c810768c03e9

Add the target triple to the compilation record.
Fixes llvm-prepo bug #44 (https://github.com/SNSystems/llvm-prepo/issues/44). Adds an additional --triple switch to pstore-dump to tell the disassembler which triple to use when disassembling a fragment (previously this was just hard-wired). Trim some unused code from the Target/X86/Repo implementation.

lib/dump/mcdisassembler_value.cpp
@@ -105,21 +105,20 @@ namespace {
         return std::max (num_bytes, 1U) * 2U + 2U;
     }
 
-    void disasm_block (std::uint8_t const * first, std::uint8_t const * last,
-                       pstore::dump::array::container & array, bool hex_mode) {
-        llvm::Triple triple ("x86_64-pc-linux-gnu-repo"); // FIXME: target triple is hardwired!
-
+    bool disasm_block (std::uint8_t const * first, std::uint8_t const * last,
+                       pstore::dump::array::container & array, pstore::gsl::czstring triple_str,
+                       bool hex_mode) {
         if (first == last) {
-            return;
+            return true;
         }
 
         // Get the target specific parser.
         std::string error;
-        std::string arch_name;
+        llvm::Triple triple{triple_str};
         llvm::Target const * const target =
-            llvm::TargetRegistry::lookupTarget (arch_name, triple, error);
+            llvm::TargetRegistry::lookupTarget ("" /*arch-name*/, triple, error);
         if (target == nullptr) {
-            pstore::raise (pstore::dump::error_code::cant_find_target, error);
+            return false; // unknown target: fall back to producing a hex-dump.
         }
 
         // Update the triple name and return the found target.
@@ -217,6 +216,19 @@ namespace {
             address += bytes_consumed;
             first += bytes_consumed;
         }
+        return true;
+    }
+
+
+    pstore::dump::value_ptr make_hex_dump_value (std::uint8_t const * first,
+                                                 std::uint8_t const * last, bool hex_mode) {
+        pstore::dump::value_ptr v;
+        if (hex_mode) {
+            v = std::make_shared<pstore::dump::binary16> (first, last);
+        } else {
+            v = std::make_shared<pstore::dump::binary> (first, last);
+        }
+        return v;
     }
 } // anonymous namespace
 
@@ -224,11 +236,13 @@ namespace pstore {
     namespace dump {
 
         value_ptr make_disassembled_value (std::uint8_t const * first, std::uint8_t const * last,
-                                           bool hex_mode) {
+                                           gsl::czstring triple, bool hex_mode) {
 
             array::container arr;
-            disasm_block (first, last, arr, hex_mode);
-            return make_value (arr);
+            if (disasm_block (first, last, arr, triple, hex_mode)) {
+                return make_value (arr);
+            }
+            return make_hex_dump_value (first, last, hex_mode);
         }
 
     } // namespace dump
@@ -240,14 +254,8 @@ namespace pstore {
     namespace dump {
 
         value_ptr make_disassembled_value (std::uint8_t const * first, std::uint8_t const * last,
-                                           bool hex_mode) {
-            value_ptr v;
-            if (hex_mode) {
-                v = std::make_shared<binary16> (first, last);
-            } else {
-                v = std::make_shared<binary> (first, last);
-            }
-            return v;
+                                           gsl::czstring triple, bool hex_mode) {
+            return make_hex_dump_value (first, last, hex_mode);
         }
 
     } // namespace dump

include/pstore/core/file_header.hpp
@@ -190,7 +190,7 @@ namespace pstore {
         std::uint32_t get_crc () const noexcept;
 
         static std::uint16_t const major_version = 0;
-        static std::uint16_t const minor_version = 5;
+        static std::uint16_t const minor_version = 6;
 
         static std::array<std::uint8_t, 4> const file_signature1;
         static std::uint32_t const file_signature2 = 0x0507FFFF;

include/pstore/dump/mcdisassembler_value.hpp
@@ -52,12 +52,14 @@
 
 #include <cstdint>
 #include "pstore/dump/value.hpp"
+#include "pstore/support/gsl.hpp"
 
 namespace pstore {
     namespace dump {
 
         value_ptr make_disassembled_value (std::uint8_t const * first, std::uint8_t const * last,
-                                           bool hex_mode);
+
+                                           gsl::czstring triple, bool hex_mode);
 
     } // namespace dump
 } // namespace pstore

include/pstore/dump/mcrepo_value.hpp
@@ -55,20 +55,23 @@ namespace pstore {
         value_ptr make_value (pstore::repo::section_kind t);
         value_ptr make_value (repo::internal_fixup const & ifx);
         value_ptr make_value (database const & db, repo::external_fixup const & xfx);
-        value_ptr make_value (database const & db, repo::generic_section const & section,
-                              repo::section_kind sk, bool hex_mode);
-        value_ptr make_value (database const & db, repo::dependents const & dependent,
-                              repo::section_kind sk, bool hex_mode);
-        value_ptr make_value (database const & db, repo::debug_line_section const & section,
-                              repo::section_kind sk, bool hex_mode);
-        value_ptr make_value (database const & db, repo::fragment const & fragment, bool hex_mode);
+
+        value_ptr make_section_value (database const & db, repo::generic_section const & section,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode);
+        value_ptr make_section_value (database const & db, repo::dependents const & dependent,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode);
+        value_ptr make_section_value (database const & db, repo::debug_line_section const & section,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode);
+
+        value_ptr make_fragment_value (database const & db, repo::fragment const & fragment,
+                                       gsl::czstring triple, bool hex_mode);
 
         value_ptr make_value (repo::linkage_type t);
         value_ptr make_value (database const & db, repo::compilation_member const & member);
         value_ptr make_value (database const & db, std::shared_ptr<repo::ticket const> ticket);
 
         value_ptr make_value (database const & db, index::fragment_index::value_type const & value,
-                              bool hex_mode);
+                              gsl::czstring triple, bool hex_mode);
         value_ptr make_value (database const & db, index::ticket_index::value_type const & value);
         value_ptr make_value (database const & db,
                               index::debug_line_header_index::value_type const & value,

include/pstore/mcrepo/ticket.hpp
@@ -143,6 +143,7 @@ namespace pstore {
             ///
             /// \param transaction  The transaction to which the ticket will be appended.
             /// \param path  A ticket file path address in the store.
+            /// \param triple  The target-triple associated with this compilation.
             /// \param first_member  The first of a sequence of ticket_member instances. The range
             ///   defined by \p first_member and \p last_member will be copied into the newly
             ///   allocated ticket.
@@ -151,8 +152,9 @@ namespace pstore {
             ///   the in-store location of the allocated ticket.
             template <typename TransactionType, typename Iterator>
             static extent<ticket> alloc (TransactionType & transaction,
-                                         typed_address<indirect_string> path, Iterator first_member,
-                                         Iterator last_member);
+                                         typed_address<indirect_string> path,
+                                         typed_address<indirect_string> triple,
+                                         Iterator first_member, Iterator last_member);
 
             /// \brief Returns a pointer to an in-pstore ticket instance.
             ///
@@ -208,20 +210,27 @@ namespace pstore {
             ///@}
 
             /// Returns the ticket file path.
-            typed_address<indirect_string> path () const { return path_addr_; }
+            typed_address<indirect_string> path () const { return path_; }
+            /// Returns the target triple.
+            typed_address<indirect_string> triple () const { return triple_; }
 
         private:
             ticket () = default;
 
-            struct nMembers {
+            struct nmembers {
                 std::size_t n;
             };
             /// A placement-new implementation which allocates sufficient storage for a
             /// ticket with the number of members given by the size parameter.
-            void * operator new (std::size_t s, nMembers size);
-            void operator delete (void * p, nMembers size);
+            void * operator new (std::size_t s, nmembers size);
+            void operator delete (void * p, nmembers size);
 
-            typed_address<indirect_string> path_addr_;
+            /// The path containing the ticket file when it was created. (Used to guide the garbage
+            /// collector's ticket-file search.)
+            typed_address<indirect_string> path_;
+            /// The target triple for this compilation.
+            typed_address<indirect_string> triple_;
+            /// The number of entries in the members_ array.
             std::uint64_t size_ = 0;
             compilation_member members_[1];
         };
@@ -233,7 +242,8 @@ namespace pstore {
         // ~~~~~
         template <typename TransactionType, typename Iterator>
         auto ticket::alloc (TransactionType & transaction, typed_address<indirect_string> path,
-                            Iterator first_member, Iterator last_member) -> extent<ticket> {
+                            typed_address<indirect_string> triple, Iterator first_member,
+                            Iterator last_member) -> extent<ticket> {
             // First work out its size.
             auto const dist = std::distance (first_member, last_member);
             assert (dist >= 0);
@@ -245,7 +255,8 @@ namespace pstore {
             auto ptr = std::static_pointer_cast<ticket> (transaction.getrw (addr, size));
 
             // Write the data to the store.
-            ptr->path_addr_ = path;
+            ptr->path_ = path;
+            ptr->triple_ = triple;
             ptr->size_ = num_members;
             std::copy (first_member, last_member, ptr->begin ());
             return pstore::extent<ticket> (typed_address<ticket> (addr), size);

lib/dump/mcrepo_value.cpp
@@ -91,8 +91,8 @@ namespace pstore {
             });
         }
 
-        value_ptr make_value (database const & db, repo::generic_section const & section,
-                              repo::section_kind sk, bool hex_mode) {
+        value_ptr make_section_value (database const & db, repo::generic_section const & section,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode) {
 
             (void) sk;
             auto const & data = section.data ();
@@ -101,7 +101,7 @@ namespace pstore {
             if (sk == repo::section_kind::text) {
                 std::uint8_t const * const first = data.data ();
                 std::uint8_t const * const last = first + data.size ();
-                data_value = make_disassembled_value (first, last, hex_mode);
+                data_value = make_disassembled_value (first, last, triple, hex_mode);
             }
 #endif
             if (!data_value) {
@@ -123,8 +123,8 @@ namespace pstore {
             });
         }
 
-        value_ptr make_value (database const & db, repo::dependents const & dependents,
-                              repo::section_kind sk, bool hex_mode) {
+        value_ptr make_section_value (database const & db, repo::dependents const & dependents,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode) {
             (void) sk;
             (void) hex_mode;
             return make_value (std::begin (dependents), std::end (dependents),
@@ -133,20 +133,22 @@ namespace pstore {
                                });
         }
 
-        value_ptr make_value (database const & db, repo::debug_line_section const & section,
-                              repo::section_kind sk, bool hex_mode) {
+        value_ptr make_section_value (database const & db, repo::debug_line_section const & section,
+                                      repo::section_kind sk, gsl::czstring triple, bool hex_mode) {
             return make_value (object::container{
                 {"header", make_value (section.header_extent ())},
-                {"generic", make_value (db, section.generic (), sk, hex_mode)},
+                {"generic", make_section_value (db, section.generic (), sk, triple, hex_mode)},
             });
         }
 
 
-        value_ptr make_value (database const & db, repo::fragment const & fragment, bool hex_mode) {
+        value_ptr make_fragment_value (database const & db, repo::fragment const & fragment,
+                                       gsl::czstring triple, bool hex_mode) {
 
 #define X(k)                                                                                       \
     case repo::section_kind::k:                                                                    \
-        contents = make_value (db, fragment.at<repo::section_kind::k> (), kind, hex_mode);         \
+        contents = make_section_value (db, fragment.at<repo::section_kind::k> (), kind, triple,    \
+                                       hex_mode);                                                  \
         break;
 
             array::container array;
@@ -197,16 +199,17 @@ namespace pstore {
             return make_value (object::container{
                 {"members", make_value (members)},
                 {"path", make_value (indirect_string::read (db, ticket->path ()))},
+                {"triple", make_value (indirect_string::read (db, ticket->triple ()))},
             });
         }
 
         value_ptr make_value (database const & db,
                               pstore::index::fragment_index::value_type const & value,
-                              bool hex_mode) {
+                              pstore::gsl::czstring triple, bool hex_mode) {
             auto fragment = pstore::repo::fragment::load (db, value.second);
-            return make_value (
-                object::container{{"digest", make_value (value.first)},
-                                  {"fragment", make_value (db, *fragment, hex_mode)}});
+            return make_value (object::container{
+                {"digest", make_value (value.first)},
+                {"fragment", make_fragment_value (db, *fragment, triple, hex_mode)}});
         }
 
         value_ptr make_value (database const & db,

lib/mcrepo/ticket.cpp
@@ -60,7 +60,7 @@ std::ostream & pstore::repo::operator<< (std::ostream & os, linkage_type l) {
 
 // operator new
 // ~~~~~~~~~~~~
-void * ticket::operator new (std::size_t s, nMembers size) {
+void * ticket::operator new (std::size_t s, nmembers size) {
     (void) s;
     std::size_t const actual_bytes = ticket::size_bytes (size.n);
     assert (actual_bytes >= s);
@@ -69,7 +69,7 @@ void * ticket::operator new (std::size_t s, nMembers size) {
 
 // operator delete
 // ~~~~~~~~~~~~~~~
-void ticket::operator delete (void * p, nMembers /*size*/) {
+void ticket::operator delete (void * p, nmembers /*size*/) {
     ::operator delete (p);
 }
 

tools/dump/main.cpp
@@ -413,7 +413,7 @@ int main (int argc, char * argv[]) {
                 file, db, show_all_fragments, opt.fragments, dump_error_code::fragment_not_found,
                 dump_error_code::no_fragment_index,
                 [&db, &opt](pstore::index::fragment_index::value_type const & value) {
-                    return make_value (db, value, opt.hex);
+                    return make_value (db, value, opt.triple.c_str (), opt.hex);
                 });
 
             show_index<pstore::trailer::indices::ticket> (

commit 1b8ad38703cddbea2dbe5f71f8794dafc89eb0f8

Fix a build-break for the standalone version.
This build fail was introduced by commit
73a22b93904d57614604604a07c1c810768c03e9. (The "inside LLVM" build was okay.)

lib/dump/mcdisassembler_value.cpp
@@ -219,6 +219,11 @@ namespace {
         return true;
     }
 
+} // anonymous namespace
+
+#endif // PSTORE_IS_INSIDE_LLVM
+
+namespace {
 
     pstore::dump::value_ptr make_hex_dump_value (std::uint8_t const * first,
                                                  std::uint8_t const * last, bool hex_mode) {
@@ -230,7 +235,10 @@ namespace {
         }
         return v;
     }
-} // anonymous namespace
+
+} // end anonymous namespace
+
+#if PSTORE_IS_INSIDE_LLVM
 
 namespace pstore {
     namespace dump {