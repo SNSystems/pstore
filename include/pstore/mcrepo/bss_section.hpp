SNSystems/pstore

commit 9e4caf112b4382b9c92e8b284012ec0e911f1a91

The size of a BSS section is size_t rather than unsigned.

include/pstore/mcrepo/bss_section.hpp
@@ -73,7 +73,7 @@ namespace pstore {
             bss_section & operator= (bss_section &&) = delete;
 
             unsigned align () const noexcept { return 1U << align_; }
-            unsigned size () const noexcept { return size_; }
+	    std::size_t size () const noexcept { return size_; }
 
             container<internal_fixup> ifixups () const { return {}; }
             container<external_fixup> xfixups () const { return {}; }

commit be7650c9c0b7736eb2f75e9e2edabeca117402f1

Fix a compile failure caused by 9e4caf112b4382b9c92e8b284012ec0e911f1a91.
bss_section::size() now returns std::uint64_t.

include/pstore/mcrepo/bss_section.hpp
@@ -73,7 +73,7 @@ namespace pstore {
             bss_section & operator= (bss_section &&) = delete;
 
             unsigned align () const noexcept { return 1U << align_; }
-	    std::size_t size () const noexcept { return size_; }
+            std::uint64_t size () const noexcept { return size_; }
 
             container<internal_fixup> ifixups () const { return {}; }
             container<external_fixup> xfixups () const { return {}; }