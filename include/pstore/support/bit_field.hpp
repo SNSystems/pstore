5f17f5bcf548aa182af6f60a46628cd20202f750
Paul Bowen-Huggett <paulhuggett@mac.com>
2018-12-03 15:57:47 +0000

    Add a portable bit-field class.
    Enables the use of bit-field like members in structures which must be
    portable between machines and compilers. (See the included
    documentation for details!)

---
+++ include/pstore/support/bit_field.hpp
@@ -0,0 +1,236 @@
+//*  _     _ _      __ _      _     _  *
+//* | |__ (_) |_   / _(_) ___| | __| | *
+//* | '_ \| | __| | |_| |/ _ \ |/ _` | *
+//* | |_) | | |_  |  _| |  __/ | (_| | *
+//* |_.__/|_|\__| |_| |_|\___|_|\__,_| *
+//*                                    *
+//===- include/pstore/support/bit_field.hpp -------------------------------===//
+// Copyright (c) 2017-2018 by Sony Interactive Entertainment, Inc.
+// All rights reserved.
+//
+// Developed by:
+//   Toolchain Team
+//   SN Systems, Ltd.
+//   www.snsystems.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal with the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimers.
+//
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimers in the
+//   documentation and/or other materials provided with the distribution.
+//
+// - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
+//   Inc. nor the names of its contributors may be used to endorse or
+//   promote products derived from this Software without specific prior
+//   written permission.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+//===----------------------------------------------------------------------===//
+/// \file bit_field.hpp
+/// \brief A portable bit-field type.
+///
+/// C and C++ bit-fields are a nice easy way to declare structure members with an explicit number of
+/// bits. However, their portability is severely limited if the exact layout of the structure is
+/// important: the language standards allow the compiler complete freedom in deciding where are how
+/// the bits are allocated in the underlying memory. We need some of the pstore data structures to
+/// be consistent regardless of the compiler being used, and there are static assertions which
+/// enforce these guarantees. Unfortunately, if we can't guarantee the bit-field layout then we
+/// can't used traditional bit-fields in these structures.
+///
+/// The template class provded here is a simple means to define a structure member which uses
+/// specified bits from a value of a specified base type.
+///
+/// Using it is simple. Define a union consisting of a member of the base type and one member for
+/// each of the bit-fields. For example:
+///
+///     union {
+///         std::uint8_t v;
+///         bit_field<std::uint8_t, 0, 2> f1; // f1 is bits [0-2)
+///         bit_field<std::uint8_t, 2, 6> f2; // f2 is bits [2-7)
+///     };
+///
+/// This is approximately equivalent to the standard C:
+///
+///     struct {
+///         std::uint8_t f1 : 2;
+///         std::uint8_t f2 : 6;
+///     };
+///
+/// The difference is that in the first example the union will occupy 8 bits with f1 being bits
+/// [0-2) and f1 beng bits [2-7). The standards make few guarantees about the second example.
+///
+/// It's worth noting that the 'v' member of the first example should be initialized to 0 _before_
+/// fields f1 or f2 are read or written. This is to ensure the masks performed by the bit-field
+/// instances don't try to read or write uninitialized memory.
+
+#ifndef PSTORE_SUPPORT_BIT_FIELD_HPP
+#define PSTORE_SUPPORT_BIT_FIELD_HPP
+
+#include <cassert>
+#include <cstddef>
+#include <cstdint>
+#include <type_traits>
+
+namespace pstore {
+
+    namespace details {
+
+        template <typename ValueType, unsigned Bits>
+        struct max_value {
+            static constexpr auto value = static_cast<ValueType> ((ValueType{1} << Bits) - 1);
+        };
+
+        template <typename ValueType>
+        struct max_value<ValueType, 8U> {
+            static constexpr auto value = static_cast<ValueType> (~std::uint8_t{0});
+        };
+        template <typename ValueType>
+        struct max_value<ValueType, 16U> {
+            static constexpr auto value = static_cast<ValueType> (~std::uint16_t{0});
+        };
+        template <typename ValueType>
+        struct max_value<ValueType, 32U> {
+            static constexpr auto value = static_cast<ValueType> (~std::uint32_t{0});
+        };
+        template <typename ValueType>
+        struct max_value<ValueType, 64U> {
+            static constexpr auto value = static_cast<ValueType> (~std::uint64_t{0});
+        };
+
+
+        template <typename ValueType, unsigned Index, unsigned Bits,
+                  typename = typename std::enable_if<std::is_unsigned<ValueType>::value &&
+                                                     Index + Bits <= sizeof (ValueType) * 8>::type>
+        class bit_field_base {
+        public:
+            static constexpr auto first_bit = Index;
+            static constexpr auto last_bit = Index + Bits;
+            /// The underlying type used to store this bit-field.
+            using value_type = ValueType;
+
+            /// Returns the smallest value that can be stored in this bit-field.
+            static constexpr value_type min () noexcept { return 0; }
+            /// Returns the largest value that can be stored in this bit-field.
+            static constexpr value_type max () noexcept { return mask_; }
+
+            /// Returns the value stored in this bit-field.
+            constexpr value_type value () const noexcept {
+                return (this->value_ >> Index) & this->mask_;
+            }
+
+            template <typename T>
+            void assign (T v) noexcept {
+                assert (std::is_unsigned<T>::value || v >= 0);
+                auto const uv = static_cast<typename std::make_unsigned<T>::type> (v);
+                assert (uv <= mask_);
+                this->assign (static_cast<value_type> (uv));
+            }
+
+            void assign (value_type v) noexcept {
+                value_ = static_cast<value_type> (value_ & ~(mask_ << Index)) |
+                         static_cast<value_type> ((v & mask_) << Index);
+            }
+
+        private:
+            static constexpr auto mask_ = max_value<ValueType, Bits>::value;
+            value_type value_;
+        };
+
+    } // end namespace details
+
+
+    template <typename ValueType, unsigned Index, unsigned Bits>
+    class bit_field : private details::bit_field_base<ValueType, Index, Bits> {
+        using inherited = details::bit_field_base<ValueType, Index, Bits>;
+
+    public:
+        using inherited::first_bit;
+        using inherited::last_bit;
+        using inherited::max;
+        using inherited::min;
+        /// The underlying type used to store this bit-field.
+        using typename inherited::value_type;
+        /// The canonical type that can be assigned to a bit-field instance.
+        using assign_type = ValueType;
+
+        /// obtains the value held in the bit-field.
+        using inherited::value;
+        /// obtains the value held in the bit-field.
+        constexpr operator value_type () const noexcept { return this->value (); }
+
+        /// assigns a value to the bit-field
+        template <typename T>
+        bit_field & operator= (T v) noexcept {
+            this->assign (v);
+            return *this;
+        }
+
+
+        template <typename T>
+        bit_field & operator+= (T other) noexcept {
+            this->assign (this->value () + other);
+            return *this;
+        }
+        template <typename T>
+        bit_field & operator-= (T other) noexcept {
+            this->assign (this->value () - other);
+            return *this;
+        }
+
+        bit_field & operator++ () noexcept { return operator+=(value_type{1}); }
+        bit_field & operator-- () noexcept { return operator-=(value_type{1}); }
+
+        bit_field operator++ (int) noexcept {
+            bit_field const prev = *this;
+            ++*this;
+            return prev;
+        }
+        bit_field operator-- (int) noexcept {
+            bit_field const prev = *this;
+            --*this;
+            return prev;
+        }
+    };
+
+    template <typename ValueType, unsigned Index>
+    class bit_field<ValueType, Index, 1> : private details::bit_field_base<ValueType, Index, 1> {
+        using inherited = details::bit_field_base<ValueType, Index, 1>;
+
+    public:
+        using inherited::first_bit;
+        using inherited::last_bit;
+        using inherited::max;
+        using inherited::min;
+        /// The underlying type used to store this bit-field.
+        using typename inherited::value_type;
+        /// The canonical type that can be assigned to a bit-field instance.
+        using assign_type = bool;
+
+        /// assigns a value to the bit-field
+        bit_field & operator= (bool v) noexcept {
+            this->assign (static_cast<value_type> (v));
+            return *this;
+        }
+
+        constexpr bool value () const noexcept { return static_cast<bool> (inherited::value ()); }
+        constexpr operator bool () const noexcept { return value (); }
+    };
+
+} // end namespace pstore
+
+#endif // PSTORE_SUPPORT_BIT_FIELD_HPP
---
+++ unittests/support/test_bit_field.cpp
@@ -0,0 +1,182 @@
+//*  _     _ _      __ _      _     _  *
+//* | |__ (_) |_   / _(_) ___| | __| | *
+//* | '_ \| | __| | |_| |/ _ \ |/ _` | *
+//* | |_) | | |_  |  _| |  __/ | (_| | *
+//* |_.__/|_|\__| |_| |_|\___|_|\__,_| *
+//*                                    *
+//===- unittests/support/test_bit_field.cpp -------------------------------===//
+// Copyright (c) 2017-2018 by Sony Interactive Entertainment, Inc.
+// All rights reserved.
+//
+// Developed by:
+//   Toolchain Team
+//   SN Systems, Ltd.
+//   www.snsystems.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal with the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+//
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimers.
+//
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimers in the
+//   documentation and/or other materials provided with the distribution.
+//
+// - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
+//   Inc. nor the names of its contributors may be used to endorse or
+//   promote products derived from this Software without specific prior
+//   written permission.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+//===----------------------------------------------------------------------===//
+#include "pstore/support/bit_field.hpp"
+#include <limits>
+#include <gtest/gtest.h>
+#include "pstore/support/bit_count.hpp"
+#include "pstore/support/portab.hpp"
+
+namespace {
+
+    template <typename T>
+    class BitFieldAssignment : public ::testing::Test {
+    public:
+    };
+
+    template <typename Type, unsigned Index, unsigned Bits>
+    struct param {
+        using value_type = Type;
+        using index = std::integral_constant<Type, Index>;
+        using bits = std::integral_constant<Type, Bits>;
+    };
+
+    using assign_test_types = ::testing::Types<
+        param<std::uint8_t, 0, 1>, // testing bits [0,1)
+        param<std::uint8_t, 1, 1>, // testing bits [1,2)
+        param<std::uint8_t, 7, 1>, // ... and so on.
+        param<std::uint16_t, 8, 1>, param<std::uint16_t, 15, 1>, param<std::uint32_t, 16, 1>,
+        param<std::uint32_t, 31, 1>, param<std::uint64_t, 32, 1>, param<std::uint64_t, 63, 1>,
+
+        param<std::uint8_t, 0, 2>, param<std::uint8_t, 1, 2>, param<std::uint8_t, 6, 2>,
+        param<std::uint16_t, 7, 2>, param<std::uint16_t, 8, 2>, param<std::uint16_t, 14, 2>,
+        param<std::uint32_t, 15, 2>, param<std::uint32_t, 16, 2>, param<std::uint64_t, 31, 2>,
+        param<std::uint64_t, 32, 2>, param<std::uint64_t, 62, 2>,
+
+        param<std::uint8_t, 0, 7>, param<std::uint8_t, 0, 8>, param<std::uint16_t, 0, 9>,
+        param<std::uint16_t, 0, 15>, param<std::uint16_t, 0, 16>, param<std::uint32_t, 0, 17>,
+        param<std::uint32_t, 0, 31>, param<std::uint32_t, 0, 32>, param<std::uint64_t, 0, 63>,
+        param<std::uint64_t, 0, 64>>;
+
+} // end anonymous namespace
+
+TYPED_TEST_CASE (BitFieldAssignment, assign_test_types);
+
+TYPED_TEST (BitFieldAssignment, Assignment) {
+    using value_type = typename TypeParam::value_type;
+    value_type const index = typename TypeParam::index ();
+    value_type const bits = typename TypeParam::bits ();
+
+    union {
+        value_type vt;
+        pstore::bit_field<value_type, index, bits> f1;
+    };
+    // set all of the bits to 1.
+    vt = std::numeric_limits<value_type>::max ();
+    // now blast values into f1.
+    f1 = 0;
+    using assign_type = typename decltype (f1)::assign_type;
+    EXPECT_EQ (f1.value (), static_cast<assign_type> (0U));
+    f1 = 1;
+    EXPECT_EQ (f1.value (), static_cast<assign_type> (1U));
+    constexpr auto v = decltype (f1)::max ();
+    EXPECT_EQ (pstore::bit_count::pop_count (v), bits);
+    f1 = v;
+    // access the value of f1 by casting directly rather than using the value() member function.
+    EXPECT_EQ (static_cast<value_type> (f1), v);
+    EXPECT_EQ (vt, std::numeric_limits<value_type>::max ());
+}
+
+TEST (BitField, IsolationFromOtherBitfields) {
+    union {
+        std::uint8_t value;
+        pstore::bit_field<std::uint8_t, 0, 2> f1; // f1 is bits [0-2)
+        pstore::bit_field<std::uint8_t, 2, 6> f2; // f2 is bits [2-8)
+    };
+
+    value = 0;
+    EXPECT_EQ (f1, 0U);
+    EXPECT_EQ (f2, 0U);
+
+    f1 = decltype (f1)::max ();
+    EXPECT_EQ (f1, decltype (f1)::max ());
+    EXPECT_EQ (f2, 0x00U);
+
+    f1 = 0U;
+    f2 = decltype (f2)::max ();
+    EXPECT_EQ (f2, decltype (f2)::max ());
+    EXPECT_EQ (f1, 0x00U);
+
+    EXPECT_EQ (value, 0xFC);
+}
+
+TEST (BitField, Addition) {
+    union {
+        std::uint8_t v;
+        pstore::bit_field<std::uint8_t, 0, 2> f1;
+    };
+    v = 0U;
+    pstore::bit_field<std::uint8_t, 0, 2> const r1 = ++f1;
+    EXPECT_EQ (f1, 1U);
+    EXPECT_EQ (r1, 1U);
+    pstore::bit_field<std::uint8_t, 0, 2> const r2 = f1++;
+    EXPECT_EQ (f1, 2U);
+    EXPECT_EQ (r2, 1U);
+    f1 = 1;
+    f1.operator+= (2U);
+    EXPECT_EQ (f1, 3U);
+    EXPECT_EQ (v, 3U);
+}
+
+TEST (BitField, Subtraction) {
+    union {
+        std::uint8_t v;
+        pstore::bit_field<std::uint8_t, 0, 2> f1;
+    };
+    v = 0U;
+    f1 = 3U;
+    pstore::bit_field<std::uint8_t, 0, 2> const r1 = --f1;
+    EXPECT_EQ (f1, 2U);
+    EXPECT_EQ (r1, 2U);
+    pstore::bit_field<std::uint8_t, 0, 2> const r2 = f1--;
+    EXPECT_EQ (f1, 1U);
+    EXPECT_EQ (r2, 2U);
+    f1 = 3U;
+    f1.operator-= (2U);
+    EXPECT_EQ (f1, 1U);
+    EXPECT_EQ (v, 1U);
+}
+
+TEST (BitField, OneBitBoolean) {
+    union {
+        std::uint8_t v;
+        pstore::bit_field<std::uint8_t, 3, 1> f1;
+    };
+    v = 0U;
+    f1 = true;
+    EXPECT_TRUE (f1.value ());
+    EXPECT_TRUE (static_cast<bool> (f1));
+    f1 = false;
+    EXPECT_FALSE (f1.value ());
+    EXPECT_FALSE (static_cast<bool> (f1));
+}
