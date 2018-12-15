702eeb93673938016bc9b89bfabf1417cf53ceb2
Paul Bowen-Huggett <paulhuggett@mac.com>
2018-12-15 16:55:47 +0000

    Simplify std:is_unsigned<>/is_signed<> for uint128.

--- include/pstore/support/uint128.hpp
+++ include/pstore/support/uint128.hpp
@@ -65,15 +65,15 @@ namespace pstore {
 
 namespace std {
     template <>
-    struct is_unsigned<pstore::uint128> : public integral_constant<bool, true> {};
+    struct is_unsigned<pstore::uint128> : true_type {};
     template <>
-    struct is_signed<pstore::uint128> : public integral_constant<bool, false> {};
+    struct is_signed<pstore::uint128> : false_type {};
 
 #if PSTORE_HAVE_UINT128_T && !PSTORE_HAVE_UINT128_TRAITS_SUPPORT
     template <>
-    struct is_unsigned<__uint128_t> : public integral_constant<bool, true> {};
+    struct is_unsigned<__uint128_t> : true_type {};
     template <>
-    struct is_signed<__uint128_t> : public integral_constant<bool, false> {};
+    struct is_signed<__uint128_t> : false_type {};
 #endif // PSTORE_HAVE_UINT128_T && !PSTORE_HAVE_UINT128_TRAITS_SUPPORT
 
 } // namespace std
