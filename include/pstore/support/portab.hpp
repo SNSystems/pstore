SNSystems/pstore

commit e79c35d3f54126f5ba2c81e9bc78f0cc264ece63

Changes for compatibility with the clang3.4 used by Klee.
Klee (http://klee.github.io) is currently built on a relatively old
version of clang (3.4): these changes are to enable the code to be
compiled. The intention is then to use Klee to improve testing for
some of the pstore APIs.

Add support for LLVM coverage if PSTORE_COVERAGE is defined.

include/pstore/support/portab.hpp
@@ -45,6 +45,15 @@
 #ifndef PSTORE_SUPPORT_PORTAB_HPP
 #define PSTORE_SUPPORT_PORTAB_HPP
 
+#include "pstore/config/config.hpp"
+
+
+/// PSTORE_CPP_EXCEPTIONS: Defined if exceptions are enabled.
+#if defined(__clang__) && !defined(__cpp_exceptions) &&                                            \
+    (defined(__EXCEPTIONS) || defined(_CPPUNWIND))
+#    define __cpp_exceptions 190000
+#endif
+
 #ifdef __cpp_exceptions
 #    define PSTORE_CPP_EXCEPTIONS __cpp_exceptions
 #elif defined(_MSC_VER) && defined(_CPPUNWIND)
@@ -94,10 +103,10 @@
 // Specifies that the function does not return.
 #if __has_cpp_attribute(noreturn)
 #    define PSTORE_NO_RETURN [[noreturn]]
+#elif PSTORE_HAVE_ATTRIBUTE_NORETURN
+#    define PSTORE_NO_RETURN __attribute__ ((noreturn))
 #elif defined(_MSC_VER)
 #    define PSTORE_NO_RETURN  __declspec(noreturn)
-#elif PSTORE_HAS_ATTRIBUTE(noreturn) && (defined(__GNUC__) || defined(__clang__))
-#    define PSTORE_NO_RETURN  __attribute__(noreturn)
 #else
 #    define PSTORE_NO_RETURN
 #endif
@@ -126,17 +135,17 @@
 #    define PSTORE_FALLTHROUGH [[fallthrough]]
 #elif __has_cpp_attribute(gnu::fallthrough)
 #    define PSTORE_FALLTHROUGH [[gnu::fallthrough]]
+#elif defined(_MSC_VER)
+// MSVC's __fallthrough annotations are checked by /analyze (Code Analysis):
+// https://msdn.microsoft.com/en-us/library/ms235402%28VS.80%29.aspx
+#    include <sal.h>
+#    define PSTORE_FALLTHROUGH __fallthrough
 #elif !__cplusplus
     // Workaround for llvm.org/PR23435, since clang 3.6 and below emit a spurious
     // error when __has_cpp_attribute is given a scoped attribute in C mode.
 #    define PSTORE_FALLTHROUGH
-#elif __has_cpp_attribute(clang::fallthrough)
+#elif __clang__ && __has_feature(cxx_attributes) && __has_warning("-Wimplicit-fallthrough")
 #    define PSTORE_FALLTHROUGH [[clang::fallthrough]]
-#elif defined(_MSC_VER)
-    // MSVC's __fallthrough annotations are checked by /analyze (Code Analysis):
-    // https://msdn.microsoft.com/en-us/library/ms235402%28VS.80%29.aspx
-#    include <sal.h>
-#    define PSTORE_FALLTHROUGH __fallthrough
 #else
 #    define PSTORE_FALLTHROUGH
 #endif

cmake/add_pstore.cmake
@@ -42,11 +42,25 @@
 # SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
 #===----------------------------------------------------------------------===//
 
+include (CheckCSourceCompiles)
+
 macro(add_pstore_subdirectory name)
     if (PSTORE_IS_INSIDE_LLVM)
         add_llvm_subdirectory(PSTORE TOOL ${name})
     endif ()
 endmacro()
+
+
+function (disable_clang_warning_if_possible target_name flag)
+    set (PSTORE_OLD_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
+    set (CMAKE_REQUIRED_FLAGS "-Werror ${flag}")
+    check_c_source_compiles ("int main () {}" PSTORE_CLANG_SUPPORTS_FLAG)
+    set (CMAKE_REQUIRED_FLAGS "${PSTORE_OLD_CMAKE_REQUIRED_FLAGS}")
+    if (PSTORE_CLANG_SUPPORTS_FLAG)
+        target_compile_options (${name} PRIVATE ${flag})
+    endif ()
+endfunction()
+
 
 function (add_pstore_additional_compiler_flag name)
     if (CMAKE_CXX_COMPILER_ID MATCHES "Clang")
@@ -61,7 +75,6 @@ function (add_pstore_additional_compiler_flag name)
             -Wno-disabled-macro-expansion
             -Wno-exit-time-destructors
             -Wno-global-constructors
-            -Wno-inconsistent-missing-override
             -Wno-missing-noreturn
             -Wno-missing-variable-declarations
             -Wno-padded
@@ -72,32 +85,24 @@ function (add_pstore_additional_compiler_flag name)
             -Wno-weak-vtables
         )
 
-        if (PSTORE_COVERAGE)
-	    # TODO: investigate llvm-cov.
-            target_compile_options (${name} PRIVATE -fprofile-arcs -ftest-coverage)
-            target_link_libraries (${name} PUBLIC --coverage)
-        endif ()
-
         # The "zero-as-null-pointer-constant" warning was added to Clang at some point.
         # Disable it if we can.
-        include (CheckCSourceCompiles)
-        set (PSTORE_OLD_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
-        set (CMAKE_REQUIRED_FLAGS "-Werror -Wno-zero-as-null-pointer-constant")
-        check_c_source_compiles ("int main () {}" PSTORE_CLANG_SUPPORTS_WNO_ZERO_AS_NULL_POINTER_CONSTANT)
-        set (CMAKE_REQUIRED_FLAGS "${PSTORE_OLD_CMAKE_REQUIRED_FLAGS}")
-        if (PSTORE_CLANG_SUPPORTS_WNO_ZERO_AS_NULL_POINTER_CONSTANT)
-            target_compile_options (${name} PRIVATE -Wno-zero-as-null-pointer-constant)
-        endif ()
+        disable_clang_warning_if_possible (${name} -Wno-zero-as-null-pointer-constant)
+
+        # The "inconsistent-missing-override" option is not available in all versions of clang.
+        # Disable it if we can.
+        # TODO: this warning is issued (at the time of writing) by gtest. It should only be disabled for that target.
+        disable_clang_warning_if_possible (${name} -Wno-inconsistent-missing-override)
 
         # Disable the "unused lambda capture" warning since we must capture
-        # extra variables for MSVC to swallow the code. Remove when VS doesn't require capture
-        set (PSTORE_OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
-        set (CMAKE_REQUIRED_FLAGS "-Werror -Wno-unused-lambda-capture")
-        check_c_source_compiles ("int main() {}" PSTORE_CLANG_SUPPORTS_NO_LAMBDA_CAPTURE)
-        set (CMAKE_REQUIRED_FLAGS ${PSTORE_OLD_CMAKE_REQUIRED_FLAGS})
-        if (PSTORE_CLANG_SUPPORTS_NO_LAMBDA_CAPTURE)
-            target_compile_options (${name} PRIVATE -Wno-unused-lambda-capture)
-        endif ()
+        # extra variables for MSVC to swallow the code. Remove when VS doesn't require capture.
+        disable_clang_warning_if_possible (${name} -Wno-unused-lambda-capture)
+
+        if (PSTORE_COVERAGE)
+            target_compile_options (${name} PRIVATE -fprofile-instr-generate -fcoverage-mapping)
+            target_compile_options (${name} PRIVATE -fno-inline-functions)
+            target_link_libraries (${name} PUBLIC -fprofile-instr-generate -fcoverage-mapping)
+        endif (PSTORE_COVERAGE)
 
     elseif (CMAKE_COMPILER_IS_GNUCXX)
 
@@ -109,7 +114,7 @@ function (add_pstore_additional_compiler_flag name)
         if (PSTORE_COVERAGE)
             target_compile_options (${name} PRIVATE -fprofile-arcs -ftest-coverage)
             target_link_libraries (${name} PUBLIC --coverage)
-        endif ()
+        endif (PSTORE_COVERAGE)
 
     elseif (MSVC)
 
@@ -127,6 +132,10 @@ function (add_pstore_additional_compiler_flag name)
             -D_CRT_SECURE_NO_WARNINGS
             -D_SCL_SECURE_NO_WARNINGS
         )
+
+        if (PSTORE_COVERAGE)
+            message (WARNING "PSTORE_COVERAGE is not yet implemented for MSVC")
+        endif (PSTORE_COVERAGE)
 
     endif ()
 endfunction(add_pstore_additional_compiler_flag)

cmake/run_pstore_unit_test.cmake
@@ -66,7 +66,8 @@ function (run_pstore_unit_test prelink_target test_target)
 
 	if (PSTORE_VALGRIND)
 	    set (OUT_LOG "${CMAKE_BINARY_DIR}/${test_target}-memcheck.log")
-            add_custom_command (TARGET ${prelink_target}
+            add_custom_command (
+                TARGET ${prelink_target}
                 PRE_LINK
                 COMMAND "${VALGRIND_PATH}"
                         --tool=memcheck --leak-check=full --show-reachable=yes
@@ -81,16 +82,31 @@ function (run_pstore_unit_test prelink_target test_target)
                 BYPRODUCTS ${OUT_LOG} ${OUT_XML}
                 VERBATIM
             )
-        else ()
-            add_custom_command (TARGET ${prelink_target}
+        elseif (PSTORE_COVERAGE)
+            add_custom_command (
+                TARGET ${prelink_target}
                 PRE_LINK
-                COMMAND ${test_target} "--gtest_output=xml:${OUT_XML}"
+                COMMAND ${CMAKE_COMMAND}
+                        -E env "LLVM_PROFILE_FILE=$<TARGET_FILE:${test_target}>.profraw"
+                        "$<TARGET_FILE:${test_target}>"
+                        "--gtest_output=xml:${OUT_XML}"
                 WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
                 COMMENT "Running ${test_target}"
                 DEPENDS ${test_target}
                 BYPRODUCTS ${OUT_XML}
                 VERBATIM
             )
-        endif (PSTORE_VALGRIND)
+        else ()
+            add_custom_command (
+                TARGET ${prelink_target}
+                PRE_LINK
+                COMMAND "${test_target}" "--gtest_output=xml:${OUT_XML}"
+                WORKING_DIRECTORY "${CMAKE_CURRENT_BINARY_DIR}"
+                COMMENT "Running ${test_target}"
+                DEPENDS ${test_target}
+                BYPRODUCTS ${OUT_XML}
+                VERBATIM
+            )
+        endif ()
     endif ()
 endfunction (run_pstore_unit_test)

include/pstore/core/address.hpp
@@ -85,8 +85,8 @@
 // (There isn't a one-to-one correspondence between regions or segments and "file mappings" (on
 // Windows). file is modified we decide if a new segment needs to be created.
 
-#ifndef PSTORE_ADDRESS_HPP
-#define PSTORE_ADDRESS_HPP
+#ifndef PSTORE_CORE_ADDRESS_HPP
+#define PSTORE_CORE_ADDRESS_HPP
 
 #include <cassert>
 #include <cstdint>
@@ -265,7 +265,7 @@ namespace pstore {
 
         typed_address () = default;
         explicit constexpr typed_address (address a) noexcept
-                : a_{a} {}
+                : a_ (std::move (a)) {}
         typed_address (typed_address const & addr) noexcept = default;
 
         template <typename Other>
@@ -453,4 +453,4 @@ namespace std {
 
 } // namespace std
 
-#endif // PSTORE_ADDRESS_HPP
+#endif // PSTORE_CORE_ADDRESS_HPP

include/pstore/diff/diff.hpp
@@ -73,7 +73,7 @@ namespace pstore {
                 traverser (database const & db, Index const & index, address threshold) noexcept
                         : db_{db}
                         , index_{index}
-                        , threshold_{threshold} {}
+                        , threshold_ (std::move (threshold)) {}
 
                 result_type operator() () const;
 

include/pstore/dump/db_value.hpp
@@ -54,7 +54,7 @@ namespace pstore {
         class address final : public value {
         public:
             explicit address (pstore::address addr)
-                    : addr_{addr}
+                    : addr_ (std::move (addr))
                     , expanded_{default_expanded_} {}
 
             bool is_number_like () const override {

include/pstore/support/sstring_view.hpp
@@ -179,8 +179,8 @@ namespace pstore {
         sstring_view (sstring_view const &) = default;
         sstring_view (sstring_view &&) noexcept = default;
         ~sstring_view () = default;
-        sstring_view & operator= (sstring_view const &) noexcept = default;
-        sstring_view & operator= (sstring_view &&) noexcept = default;
+        sstring_view & operator= (sstring_view const &) = default;
+        sstring_view & operator= (sstring_view &&) = default;
 
         // 7.4, sstring_view iterator support
         const_iterator begin () const noexcept { return data (); }

lib/support/CMakeLists.txt
@@ -62,6 +62,15 @@ set (CMAKE_REQUIRED_LIBRARIES )
 
 
 check_cxx_source_compiles (
+    "#include <cstdlib>
+    __attribute__((noreturn)) int f () {
+        std::exit(0);
+    }
+    int main () {}"
+    PSTORE_HAVE_ATTRIBUTE_NORETURN
+)
+
+check_cxx_source_compiles (
     "int main () {
         __uint128_t a;
     }"

commit 20ab0066d67eaed2f3da0a64a3178d07cc309fc5

Add __hasfeature,__has_extension,__has_warning for older compilers.
Change e79c35d3f54126f5ba2c81e9bc78f0cc264ece63 broke compatibility
with GCC5.5 which doesn't support one or more of the above macros. Add
a fallback for this case.

include/pstore/support/portab.hpp
@@ -95,9 +95,9 @@
 /// nonzero constant integer if the attribute is supported or 0 if not.
 /// It evaluates to zero if `__has_attribute` is not defined by the compiler.
 #ifdef __has_attribute
-#define PSTORE_HAS_ATTRIBUTE(x) __has_attribute(x)
+#    define PSTORE_HAS_ATTRIBUTE(x) __has_attribute (x)
 #else
-#define PSTORE_HAS_ATTRIBUTE(x)  false
+#    define PSTORE_HAS_ATTRIBUTE(x) false
 #endif
 
 // Specifies that the function does not return.
@@ -106,7 +106,7 @@
 #elif PSTORE_HAVE_ATTRIBUTE_NORETURN
 #    define PSTORE_NO_RETURN __attribute__ ((noreturn))
 #elif defined(_MSC_VER)
-#    define PSTORE_NO_RETURN  __declspec(noreturn)
+#    define PSTORE_NO_RETURN __declspec(noreturn)
 #else
 #    define PSTORE_NO_RETURN
 #endif
@@ -125,9 +125,20 @@
 #endif
 
 #ifndef PSTORE_STATIC_ASSERT
-    /// A single-argument version of static_assert. Remove once we can run the compiler in C++14
-    /// mode.
+/// A single-argument version of static_assert. Remove once we can run the compiler in C++14
+/// mode.
 #    define PSTORE_STATIC_ASSERT(x) static_assert (x, #    x)
+#endif
+
+#ifndef __has_feature          // Optional of course.
+#    define __has_feature(x) 0 // Compatibility with non-clang compilers.
+#endif
+#ifndef __has_extension
+#    define __has_extension __has_feature // Compatibility with pre-3.0 compilers.
+#endif
+
+#ifndef __has_warning
+#    define __has_warning(x) 0 // Compatibility with non-clang compilers.
 #endif
 
 /// PSTORE_FALLTHROUGH - Mark fallthrough cases in switch statements.
@@ -136,13 +147,13 @@
 #elif __has_cpp_attribute(gnu::fallthrough)
 #    define PSTORE_FALLTHROUGH [[gnu::fallthrough]]
 #elif defined(_MSC_VER)
-// MSVC's __fallthrough annotations are checked by /analyze (Code Analysis):
-// https://msdn.microsoft.com/en-us/library/ms235402%28VS.80%29.aspx
+    // MSVC's __fallthrough annotations are checked by /analyze (Code Analysis):
+    // https://msdn.microsoft.com/en-us/library/ms235402%28VS.80%29.aspx
 #    include <sal.h>
 #    define PSTORE_FALLTHROUGH __fallthrough
 #elif !__cplusplus
-    // Workaround for llvm.org/PR23435, since clang 3.6 and below emit a spurious
-    // error when __has_cpp_attribute is given a scoped attribute in C mode.
+// Workaround for llvm.org/PR23435, since clang 3.6 and below emit a spurious
+// error when __has_cpp_attribute is given a scoped attribute in C mode.
 #    define PSTORE_FALLTHROUGH
 #elif __clang__ && __has_feature(cxx_attributes) && __has_warning("-Wimplicit-fallthrough")
 #    define PSTORE_FALLTHROUGH [[clang::fallthrough]]