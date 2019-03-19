987b9bb9a9f2908664e66b8cbaa2c9a4d0feedef
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-03-19 14:18:41 +0100

    Split the server loop up and modernize the code.
    With the intention of simplifying and modularizing the monolithic
    server loop this change splits some of it out into separate
    functions. At the same time, some code has been modernized and simplified.

--- lib/httpd/server.cpp
+++ lib/httpd/server.cpp
@@ -46,6 +46,7 @@
 #include <cstdio>
 #include <cstring>
 #include <iostream>
+#include <limits>
 #include <sstream>
 #include <string>
 
@@ -112,7 +113,9 @@ namespace {
     void error (char const * msg) { std::cerr << msg << '(' << get_last_error () << ")\n"; }
 
     void send (socket_descriptor const & socket, void const * ptr, std::size_t size) {
-        if (::send (socket.get (), static_cast<char const *> (ptr), size, 0 /*flags*/) < 0) {
+        assert (size <= std::numeric_limits<int>::max ());
+        if (::send (socket.get (), static_cast<char const *> (ptr), static_cast<int> (size),
+                    0 /*flags*/) < 0) {
             error ("ERROR on send");
         }
     }
@@ -152,13 +155,117 @@ namespace {
     refiller (socket_descriptor & socket, pstore::gsl::span<char> const & s) {
         using result_type =
             pstore::error_or<std::pair<socket_descriptor &, pstore::gsl::span<char>::iterator>>;
+
         auto const size = s.size ();
-        ssize_t const nread = ::recv (socket.get (), s.data (), size, 0 /*flags*/);
-        assert (is_recv_error (nread) || nread <= size);
+        assert (size < std::numeric_limits<int>::max ());
+        ssize_t const nread =
+            ::recv (socket.get (), s.data (), static_cast<int> (size), 0 /*flags*/);
+        assert (is_recv_error (nread) || (nread >= 0 && nread <= size));
         if (is_recv_error (nread)) {
             return result_type{get_last_error ()};
         }
         return result_type{pstore::in_place, socket, s.begin () + nread};
+    }
+
+
+    // initialize_socket
+    // ~~~~~~~~~~~~~~~~~
+    pstore::error_or<socket_descriptor> initialize_socket (in_port_t port_number) {
+        using eo = pstore::error_or<socket_descriptor>;
+
+        socket_descriptor fd{::socket (AF_INET, SOCK_STREAM, 0)};
+        if (!fd.valid ()) {
+            return eo{get_last_error ()};
+        }
+
+        int const optval = 1;
+        if (::setsockopt (fd.get (), SOL_SOCKET, SO_REUSEADDR,
+                          reinterpret_cast<char const *> (&optval), sizeof (optval))) {
+            return eo{get_last_error ()};
+        }
+
+        sockaddr_in serveraddr; // server's addr.
+        std::memset (&serveraddr, 0, sizeof (serveraddr));
+        serveraddr.sin_family = AF_INET;
+        serveraddr.sin_addr.s_addr = htonl (INADDR_ANY);
+        serveraddr.sin_port = htons (port_number);
+        if (::bind (fd.get (), reinterpret_cast<sockaddr *> (&serveraddr), sizeof (serveraddr)) <
+            0) {
+            return eo{get_last_error ()};
+        }
+
+        // Get ready to accept connection requests.
+        if (::listen (fd.get (), 5) < 0) { // allow 5 requests to queue up.
+            return eo{get_last_error ()};
+        }
+
+        return eo{pstore::in_place, std::move (fd)};
+    }
+
+    struct request_info {
+        request_info (std::string m, std::string v, std::string u)
+                : method{std::move (m)}
+                , version{std::move (v)}
+                , uri{std::move (u)} {}
+        request_info (request_info const &) = default;
+        request_info (request_info &&) = default;
+
+        request_info & operator= (request_info const &) = delete;
+        request_info & operator= (request_info &&) = delete;
+
+        std::string const method;
+        std::string const version;
+        std::string const uri;
+    };
+
+    // read_request
+    // ~~~~~~~~~~~~
+    template <typename ReaderType>
+    pstore::error_or<request_info> read_request (ReaderType & reader, socket_descriptor & socket) {
+        using pstore::error_or;
+        using pstore::maybe;
+
+        auto check_for_eof = [](std::pair<socket_descriptor &, maybe<std::string>> const & p) {
+            auto const & buf = std::get<1> (p);
+            if (!buf) {
+                // TODO: use a more suitable error.
+                return error_or<std::string>{std::make_error_code (std::errc (ENOTCONN))};
+            }
+            return error_or<std::string>{pstore::in_place, *buf};
+        };
+
+        auto extract_request_info = [](std::string const & s) {
+            std::istringstream str{s};
+            std::string method;
+            std::string version;
+            std::string uri;
+            str >> method >> uri >> version;
+            return error_or<request_info>{pstore::in_place, method, version, uri};
+        };
+
+        return (reader.gets (socket) >>= check_for_eof) >>= extract_request_info;
+    }
+
+    // read_headers
+    // ~~~~~~~~~~~~
+    template <typename ReaderType, typename HandleFn>
+    ReaderType & read_headers (ReaderType & reader, socket_descriptor & childfd, HandleFn handler) {
+        using pstore::error_or;
+        using pstore::maybe;
+
+        error_or<std::pair<socket_descriptor &, maybe<std::string>>> const es =
+            reader.gets (childfd);
+        if (es.has_error ()) {
+            std::cerr << "Error on recv: " << es.get_error () << '\n';
+            return reader;
+        }
+        assert (std::get<0> (es.get_value ()) == childfd);
+        std::string const & s = std::get<1> (es.get_value ()).value ();
+        if (s.length () == 0) {
+            return reader;
+        }
+        handler (s);
+        return read_headers (reader, childfd, handler);
     }
 
 } // end anonymous namespace
@@ -167,44 +274,14 @@ namespace pstore {
     namespace httpd {
 
         int server (in_port_t port_number, romfs::romfs & file_system) {
-            // open socket descriptor.
-            socket_descriptor parentfd{::socket (AF_INET, SOCK_STREAM, 0)};
-            if (!parentfd.valid ()) {
+            pstore::error_or<socket_descriptor> eparentfd = initialize_socket (port_number);
+            if (eparentfd.has_error ()) {
                 error ("ERROR opening socket");
             }
+            socket_descriptor const & parentfd = eparentfd.get_value ();
 
-            {
-                // Allows us to restart the server immediately.
-                int const optval = 1;
-                if (::setsockopt (parentfd.get (), SOL_SOCKET, SO_REUSEADDR,
-                                  reinterpret_cast<char const *> (&optval), sizeof (optval))) {
-                    error ("ERROR on setsockopt");
-                }
-            }
-
-            {
-                // Bind port to socket.
-                sockaddr_in serveraddr; // server's addr.
-                std::memset (&serveraddr, 0, sizeof (serveraddr));
-                serveraddr.sin_family = AF_INET;
-                serveraddr.sin_addr.s_addr = htonl (INADDR_ANY);
-                serveraddr.sin_port = htons (port_number);
-                if (::bind (parentfd.get (), reinterpret_cast<sockaddr *> (&serveraddr),
-                            sizeof (serveraddr)) < 0) {
-                    error ("ERROR on binding");
-                }
-            }
-
-            /* get us ready to accept connection requests */
-            // FIXME: socket_error on win32
-            if (::listen (parentfd.get (), 5) < 0) { /* allow 5 requests to queue up */
-                error ("ERROR on listen");
-            }
-
-            /*
-             * main loop: wait for a connection request, parse HTTP,
-             * serve requested content, close connection.
-             */
+            // main loop: wait for a connection request, parse HTTP,
+            // serve requested content, close connection.
             sockaddr_in clientaddr; // client address.
             auto clientlen =
                 static_cast<socklen_t> (sizeof (clientaddr)); // byte size of client's address
@@ -217,78 +294,63 @@ namespace pstore {
                               &clientlen)};
                 if (!childfd.valid ()) {
                     error ("ERROR on accept");
+                    continue;
                 }
 
                 // Determine who sent the message.
-                hostent * const hostp =
-                    gethostbyaddr (reinterpret_cast<char const *> (&clientaddr.sin_addr.s_addr),
-                                   sizeof (clientaddr.sin_addr.s_addr), AF_INET);
-                if (hostp == nullptr) {
-                    error ("ERROR on gethostbyaddr");
+                std::array<char, 64> host_name;
+                int const gni_err = ::getnameinfo (
+                    reinterpret_cast<sockaddr *> (&clientaddr), sizeof (clientaddr),
+                    host_name.data (), host_name.size (), nullptr, socklen_t{0}, 0 /*flags*/);
+                if (gni_err != 0) {
+                    error ("ERROR on getnameinfo");
+                    continue;
                 }
+                host_name.back () = '\0'; // guarantee nul termination.
+                std::cout << "-- Connection from " << host_name.data () << '\n';
+
+#if 0
                 char const * const hostaddrp = inet_ntoa (clientaddr.sin_addr);
                 if (hostaddrp == nullptr) {
                     error ("ERROR on inet_ntoa\n");
+                    continue;
                 }
+#endif
 
+                // Get the HTTP request line.
                 auto reader = make_buffered_reader<socket_descriptor &> (refiller);
 
-                /* get the HTTP request line */
-                constexpr std::size_t buffer_size = 1024;
-                char method[buffer_size];  // request method
-                char version[buffer_size]; // request method
-                char uri[buffer_size];     // request uri
-                {
-                    error_or<std::pair<socket_descriptor &, maybe<std::string>>> const ebuf =
-                        reader.gets (childfd);
-                    if (ebuf.has_error ()) {
-                        std::cerr << "Error on recv: " << ebuf.get_error () << '\n';
-                        continue;
-                    }
-
-                    assert (std::get<0> (ebuf.get_value ()) == childfd);
-                    maybe<std::string> const & buf = std::get<1> (ebuf.get_value ());
-                    if (!buf) {
-                        continue;
-                    }
-
-                    std::cout << *buf << '\n';
-                    std::sscanf (buf->c_str (), "%s %s %s\n", method, uri, version);
+                pstore::error_or<request_info> eri = read_request (reader, childfd);
+                if (eri.has_error ()) {
+                    std::cerr << "Error when reading HTTP request: " << eri.get_error () << '\n';
+                    continue;
                 }
 
+                request_info const & request = eri.get_value ();
+
                 // We  only currently support the GET method.
-                if (std::strcmp (method, "GET") != 0) {
-                    cerror (childfd, method, "501", "Not Implemented",
+                if (request.method != "GET") {
+                    cerror (childfd, request.method.c_str (), "501", "Not Implemented",
                             "httpd does not implement this method");
                     continue;
                 }
 
-                // read (and for the moment ignore) the HTTP headers.
-                {
-                    std::string s;
-                    do {
-                        error_or<std::pair<socket_descriptor &, maybe<std::string>>> const es =
-                            reader.gets (childfd);
-                        if (es.has_error ()) {
-                            std::cerr << "Error on recv: " << es.get_error () << '\n';
-                            continue;
-                        }
-                        assert (std::get<0> (es.get_value ()) == childfd);
-                        s = std::get<1> (es.get_value ()).value ();
-                        std::cout << s << '\n';
-                    } while (s.length () > 0);
-                }
+                // read (and for the moment simply print) the HTTP headers.
+                read_headers (reader, childfd, [](std::string const & header) {
+                    std::cout << "  " << header << '\n';
+                });
 
                 /* parse the uri [crufty] */
                 bool is_static = true; /* static request? */
                 std::string filename;
+                constexpr std::size_t buffer_size = 1024;
                 char cgiargs[buffer_size];      /* cgi argument list */
-                if (!strstr (uri, "cgi-bin")) { /* static content */
+                if (!strstr (request.uri.c_str (), "cgi-bin")) { /* static content */
                     is_static = true;
                     cgiargs[0] = '\0';
                     filename = ".";
-                    filename += uri;
-                    if (uri[std::strlen (uri) - 1] == '/') {
+                    filename += request.uri;
+                    if (request.uri.back () == '/') {
                         filename += "index.html";
                     }
                 } else { /* dynamic content */
@@ -310,7 +372,6 @@ namespace pstore {
                 if (sbuf.has_error ()) {
                     cerror (childfd, filename.c_str (), "404", "Not found",
                             "pstore-httpd couldn't find this file");
-
                     childfd.reset ();
                     continue;
                 }
