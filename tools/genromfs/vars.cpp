72cdd87b8ad8bdaad044481c1085edcdbaa9ed6b
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-04-15 09:21:28 +0200

    This files doesn't need to include <ostream>.

--- tools/genromfs/vars.cpp
+++ tools/genromfs/vars.cpp
@@ -42,7 +42,6 @@
 // SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
 //===----------------------------------------------------------------------===//
 #include "vars.hpp"
-#include <ostream>
 
 std::string const directory_var_policy::name_ = "d";
 std::string const file_var_policy::name_ = "f";
