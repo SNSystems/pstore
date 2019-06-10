109927615d256e48e4e8b9465e210b50f87d328c
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-06-10 12:14:37 +0100

    Eliminate some duplication in the renderer code.

--- tools/broker_ui/renderer.js
+++ tools/broker_ui/renderer.js
@@ -4,30 +4,30 @@
 
 var localhost = 'localhost:8080';
 // var localhost = window.location.host;
-var new_ws = function (channel, on_message) {
+function new_ws (channel, on_message) {
     var socket = new WebSocket ('ws://' + localhost + '/' + channel);
     socket.onerror = error => { console.error(error); };
     socket.onopen = () => { console.log('connection open (' + channel + ')'); };
     socket.onclose = () => { console.log('connection closed (' + channel + ')'); };
     socket.onmessage = on_message;
     return socket;
-};
-
-var uptime_socket = new_ws ('uptime', msg => {
-    var obj = JSON.parse(msg.data);
-    var el = document.getElementById('message');
-    console.log(obj.uptime);
-    el.textContent = obj.uptime;
-});
+}
 
 var commits = 0;
-var commit_socket = new_ws ('commits', msg => {
+
+function message_received (msg) {
     var obj = JSON.parse(msg.data);
-    var el = document.getElementById('commits');
-    console.log(obj.commits);
-    commits = obj.commits;
-    el.textContent = commits;
-});
+    if (obj.uptime != undefined) {
+        document.getElementById('uptime').textContent = obj.uptime;
+    }
+    if (obj.commits != undefined) {
+        commits = obj.commits;
+        document.getElementById('commits').textContent = obj.commits;
+    }
+}
+
+var uptime_socket = new_ws ('uptime', message_received);
+var commit_socket = new_ws ('commits', message_received);
 
 var prev_commits = 0;
 setInterval (() => {
