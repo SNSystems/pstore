099da09a31f436057d61bb98594c663b3f6fdd46
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-07-19 17:40:00 +0200

    Replace a couple of explicit throws with raise().

--- lib/os/thread_win32.cpp
+++ lib/os/thread_win32.cpp
@@ -70,41 +70,41 @@ namespace pstore {
             // actually stores the thread -> name mapping. Windows itself has no
             // notion of a thread "Name".
             if (name == nullptr) {
-                throw std::system_error (EINVAL, std::generic_category ());
-            } else {
-                std::strncpy (thread_name, name, name_size);
-                thread_name[name_size - 1] = '\0';
+                raise (errno_erc{EINVAL});
+            }
+
+            std::strncpy (thread_name, name, name_size);
+            thread_name[name_size - 1] = '\0';
 #    ifndef NDEBUG
-                DWORD const MS_VC_EXCEPTION = 0x406D1388;
+            DWORD const MS_VC_EXCEPTION = 0x406D1388;
 
 #        pragma pack(push, 8)
-                struct THREADNAME_INFO {
-                    DWORD dwType;     // Must be 0x1000.
-                    LPCSTR szName;    // Pointer to name (in user addr space).
-                    DWORD dwThreadID; // Thread ID (-1=caller thread).
-                    DWORD dwFlags;    // Reserved for future use, must be zero.
-                };
+            struct THREADNAME_INFO {
+                DWORD dwType;     // Must be 0x1000.
+                LPCSTR szName;    // Pointer to name (in user addr space).
+                DWORD dwThreadID; // Thread ID (-1=caller thread).
+                DWORD dwFlags;    // Reserved for future use, must be zero.
+            };
 #        pragma pack(pop)
 
-                THREADNAME_INFO info;
-                info.dwType = 0x1000;
-                info.szName = name;
-                info.dwThreadID = GetCurrentThreadId ();
-                info.dwFlags = 0;
+            THREADNAME_INFO info;
+            info.dwType = 0x1000;
+            info.szName = name;
+            info.dwThreadID = GetCurrentThreadId ();
+            info.dwFlags = 0;
 
-                __try {
-                    RaiseException (MS_VC_EXCEPTION, 0, sizeof (info) / sizeof (ULONG_PTR),
-                                    (ULONG_PTR *) &info);
-                } __except (EXCEPTION_EXECUTE_HANDLER) {
-                }
-#    endif // NDEBUG
+            __try {
+                RaiseException (MS_VC_EXCEPTION, 0, sizeof (info) / sizeof (ULONG_PTR),
+                                (ULONG_PTR *) &info);
+            } __except (EXCEPTION_EXECUTE_HANDLER) {
             }
+#    endif // NDEBUG
         }
 
         char const * get_name (gsl::span<char, name_size> name /*out*/) {
             auto const length = name.size ();
             if (name.data () == nullptr || length < 1) {
-                throw std::system_error (EINVAL, std::generic_category ());
+                raise (errno_erc{EINVAL});
             }
             std::strncpy (name.data (), thread_name, length);
             name[length - 1] = '\0';
