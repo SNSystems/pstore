167863c7513f5626d5b38b968e66e5e7d2925f6b
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-08-06 18:06:46 +0200

    Report an error if either fread() or snprintf() fails.

--- tools/genromfs/copy.cpp
+++ tools/genromfs/copy.cpp
@@ -47,6 +47,7 @@
 #include <array>
 #include <iostream>
 #include <sstream>
+#include <stdexcept>
 
 // pstore includes
 #include "pstore/support/array_elements.hpp"
@@ -72,12 +73,40 @@ namespace {
 
 #endif
 
+    PSTORE_NO_RETURN void open_failed (int erc, std::string const & path) {
+        std::stringstream str;
+        str << "fopen " << pstore::quoted (path);
+        raise (pstore::errno_erc{erc}, str.str ());
+    }
+
+    class snprintf_failed_error : public std::runtime_error {
+    public:
+        snprintf_failed_error ()
+                : std::runtime_error ("snprintf failed") {}
+    };
+
+    class read_failed_error : public std::runtime_error {
+    public:
+        explicit read_failed_error (std::string const & message)
+                : std::runtime_error (message) {}
+    };
+
+    PSTORE_NO_RETURN void read_failed (std::string const & path) {
+        std::stringstream str;
+        str << "read of file " << pstore::quoted (path) << " failed";
+        pstore::raise_exception (read_failed_error{str.str ()});
+    }
+
 } // end anonymous namespace
 
 void copy (std::string const & path, unsigned file_no) {
     static constexpr auto indent_size = pstore::array_elements (indent) - 1U;
     static constexpr auto crindent_size = pstore::array_elements (crindent) - 1U;
     static constexpr auto line_width = std::size_t{80} - indent_size;
+    static constexpr auto separator_size = std::size_t{1};  // empty or comma
+    static constexpr auto byte_value_size = std::size_t{3}; // base10: 0-255.
+
+    std::ostream & os = std::cout;
 
     auto getcr = [](std::size_t width) {
         return width >= line_width ? std::make_pair (std::size_t{0}, crindent)
@@ -88,12 +117,9 @@ void copy (std::string const & path, unsigned file_no) {
     std::uint8_t buffer[buffer_size] = {0};
     std::unique_ptr<FILE, decltype (&std::fclose)> file (file_open (path), &std::fclose);
     if (!file) {
-        int const erc = errno;
-        std::stringstream str;
-        str << "fopen " << pstore::quoted (path);
-        raise (pstore::errno_erc{erc}, str.str ());
+        open_failed (errno, path);
     }
-    std::cout << "std::uint8_t const " << file_var (file_no) << "[] = {\n" << indent;
+    os << "std::uint8_t const " << file_var (file_no) << "[] = {\n" << indent;
     std::size_t width = indent_size;
     char const * separator = "";
     auto num_read = std::size_t{0};
@@ -101,27 +127,28 @@ void copy (std::string const & path, unsigned file_no) {
         num_read = std::fread (&buffer[0], sizeof (buffer[0]), buffer_size, file.get ());
         num_read = std::min (buffer_size, num_read);
         if (std::ferror (file.get ())) {
-            // FIXME: report an error
-            return;
+            read_failed (path);
         }
         for (auto n = std::size_t{0}; n < num_read; ++n) {
             char const * cr;
             std::tie (width, cr) = getcr (width);
 
-            std::array<char, 1 + crindent_size + 3 + 1> vbuf{{0}};
+            assert (std::strlen (separator) <= 1);
+            std::array<char, separator_size + crindent_size + byte_value_size + 1> vbuf{{0}};
             int written = std::snprintf (vbuf.data (), vbuf.size (), "%s%s%u", separator, cr,
                                          static_cast<unsigned> (buffer[n]));
             if (written < 0) {
-                // TODO: an error occurred. Do something about it.
+                // Is there anything more sensible we can do?
+                pstore::raise_exception (snprintf_failed_error ());
             }
             assert (vbuf[vbuf.size () - 1U] == '\0' && "vbuf was not big enough");
             assert (static_cast<unsigned> (written) <= vbuf.size ());
             // Absolute guarantee of nul termination.
             vbuf[vbuf.size () - 1U] = '\0';
-            std::cout << vbuf.data ();
+            os << vbuf.data ();
             width += static_cast<std::make_unsigned<decltype (written)>::type> (written);
             separator = ",";
         }
     } while (num_read >= buffer_size);
-    std::cout << "\n};\n";
+    os << "\n};\n";
 }
