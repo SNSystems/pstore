2342e96885512b3a10f09a1d95f2bb3153e90a9b
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-09-17 14:44:51 +0200

    Re-run the boilerplate utility.

--- utils/boilerplate/all.py
+++ utils/boilerplate/all.py
@@ -1,47 +1,47 @@
 #!/usr/bin/env python
-#*        _ _  *
-#*   __ _| | | *
-#*  / _` | | | *
-#* | (_| | | | *
-#*  \__,_|_|_| *
-#*             *
-#===- utils/boilerplate/all.py --------------------------------------------===//
-# Copyright (c) 2017-2019 by Sony Interactive Entertainment, Inc.
-# All rights reserved.
+# *        _ _  *
+# *   __ _| | | *
+# *  / _` | | | *
+# * | (_| | | | *
+# *  \__,_|_|_| *
+# *             *
+# ===- utils/boilerplate/all.py -------------------------------------------===//
+#  Copyright (c) 2017-2019 by Sony Interactive Entertainment, Inc.
+#  All rights reserved.
 #
-# Developed by:
-#   Toolchain Team
-#   SN Systems, Ltd.
-#   www.snsystems.com
+#  Developed by:
+#    Toolchain Team
+#    SN Systems, Ltd.
+#    www.snsystems.com
 #
-# Permission is hereby granted, free of charge, to any person obtaining a
-# copy of this software and associated documentation files (the
-# "Software"), to deal with the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the
+#  "Software"), to deal with the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
 #
-# - Redistributions of source code must retain the above copyright notice,
-#   this list of conditions and the following disclaimers.
+#  - Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimers.
 #
-# - Redistributions in binary form must reproduce the above copyright
-#   notice, this list of conditions and the following disclaimers in the
-#   documentation and/or other materials provided with the distribution.
+#  - Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimers in the
+#    documentation and/or other materials provided with the distribution.
 #
-# - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
-#   Inc. nor the names of its contributors may be used to endorse or
-#   promote products derived from this Software without specific prior
-#   written permission.
+#  - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
+#    Inc. nor the names of its contributors may be used to endorse or
+#    promote products derived from this Software without specific prior
+#    written permission.
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
-# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
-# ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
-#===----------------------------------------------------------------------===//
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+#  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+# ===----------------------------------------------------------------------===//
 """
 A simple utility which applies standard "boilerplate" to an entire directory tree of files. Only files whose
 names match on of a set of globbing patterns will be modified.
