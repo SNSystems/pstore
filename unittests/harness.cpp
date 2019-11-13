a55b98a015362874b3b0ac6441e16fcfc9a5158c
Paul Bowen-Huggett <paulhuggett@mac.com>
2019-11-13 16:55:14 +0000

    Add a "loud" mode to the test harness which restores gtest's output.

--- unittests/harness.cpp
+++ unittests/harness.cpp
@@ -71,71 +71,84 @@
 #    define PSTORE_CATCH(x, code)
 #endif
 
-class quiet_event_listener : public testing::TestEventListener {
-public:
-    explicit quiet_event_listener (TestEventListener * listener);
-    quiet_event_listener (quiet_event_listener const &) = delete;
-    quiet_event_listener (quiet_event_listener &&) = delete;
-    ~quiet_event_listener () override = default;
+namespace {
 
-    quiet_event_listener & operator= (quiet_event_listener const &) = delete;
-    quiet_event_listener & operator= (quiet_event_listener &&) = delete;
-
-    void OnTestProgramStart (testing::UnitTest const & test) override;
-    void OnTestIterationStart (testing::UnitTest const & test, int iteration) override;
-    void OnEnvironmentsSetUpStart (testing::UnitTest const & test) override;
-    void OnEnvironmentsSetUpEnd (testing::UnitTest const & test) override;
-    void OnTestCaseStart (testing::TestCase const & test_case) override;
-    void OnTestStart (testing::TestInfo const & test_info) override;
-    void OnTestPartResult (testing::TestPartResult const & result) override;
-    void OnTestEnd (testing::TestInfo const & test_info) override;
-    void OnTestCaseEnd (testing::TestCase const & test_case) override;
-    void OnEnvironmentsTearDownStart (testing::UnitTest const & test) override;
-    void OnEnvironmentsTearDownEnd (testing::UnitTest const & test) override;
-    void OnTestIterationEnd (testing::UnitTest const & test, int iteration) override;
-    void OnTestProgramEnd (testing::UnitTest const & test) override;
-
-private:
-    std::unique_ptr<testing::TestEventListener> listener_;
-};
-
-quiet_event_listener::quiet_event_listener (TestEventListener * listener)
-        : listener_ (listener) {}
-
-void quiet_event_listener::OnTestProgramStart (testing::UnitTest const & test) {
-    listener_->OnTestProgramStart (test);
-}
-
-void quiet_event_listener::OnTestIterationStart (testing::UnitTest const & test, int iteration) {
-    listener_->OnTestIterationStart (test, iteration);
-}
-
-void quiet_event_listener::OnEnvironmentsSetUpStart (testing::UnitTest const &) {}
-void quiet_event_listener::OnEnvironmentsSetUpEnd (testing::UnitTest const &) {}
-void quiet_event_listener::OnEnvironmentsTearDownStart (testing::UnitTest const &) {}
-void quiet_event_listener::OnEnvironmentsTearDownEnd (testing::UnitTest const &) {}
-
-void quiet_event_listener::OnTestCaseStart (testing::TestCase const &) {}
-void quiet_event_listener::OnTestCaseEnd (testing::TestCase const &) {}
-void quiet_event_listener::OnTestStart (testing::TestInfo const &) {}
-
-void quiet_event_listener::OnTestPartResult (testing::TestPartResult const & result) {
-    listener_->OnTestPartResult (result);
-}
-
-void quiet_event_listener::OnTestEnd (testing::TestInfo const & test_info) {
-    if (test_info.result ()->Failed ()) {
-        listener_->OnTestEnd (test_info);
+    bool const loud_mode_enabled (int argc, char ** argv) {
+        if (argc < 2) {
+            return false;
+        }
+        return std::any_of (&argv[1], argv + argc,
+                            [] (char const * argc) { return std::strcmp (argc, "--loud") == 0; });
     }
-}
 
-void quiet_event_listener::OnTestIterationEnd (testing::UnitTest const & test, int iteration) {
-    listener_->OnTestIterationEnd (test, iteration);
-}
+    class quiet_event_listener : public testing::TestEventListener {
+    public:
+        explicit quiet_event_listener (TestEventListener * listener);
+        quiet_event_listener (quiet_event_listener const &) = delete;
+        quiet_event_listener (quiet_event_listener &&) = delete;
+        ~quiet_event_listener () override = default;
 
-void quiet_event_listener::OnTestProgramEnd (testing::UnitTest const & test) {
-    listener_->OnTestProgramEnd (test);
-}
+        quiet_event_listener & operator= (quiet_event_listener const &) = delete;
+        quiet_event_listener & operator= (quiet_event_listener &&) = delete;
+
+        void OnTestProgramStart (testing::UnitTest const & test) override;
+        void OnTestIterationStart (testing::UnitTest const & test, int iteration) override;
+        void OnEnvironmentsSetUpStart (testing::UnitTest const & test) override;
+        void OnEnvironmentsSetUpEnd (testing::UnitTest const & test) override;
+        void OnTestCaseStart (testing::TestCase const & test_case) override;
+        void OnTestStart (testing::TestInfo const & test_info) override;
+        void OnTestPartResult (testing::TestPartResult const & result) override;
+        void OnTestEnd (testing::TestInfo const & test_info) override;
+        void OnTestCaseEnd (testing::TestCase const & test_case) override;
+        void OnEnvironmentsTearDownStart (testing::UnitTest const & test) override;
+        void OnEnvironmentsTearDownEnd (testing::UnitTest const & test) override;
+        void OnTestIterationEnd (testing::UnitTest const & test, int iteration) override;
+        void OnTestProgramEnd (testing::UnitTest const & test) override;
+
+    private:
+        std::unique_ptr<testing::TestEventListener> listener_;
+    };
+
+    quiet_event_listener::quiet_event_listener (TestEventListener * listener)
+            : listener_ (listener) {}
+
+    void quiet_event_listener::OnTestProgramStart (testing::UnitTest const & test) {
+        listener_->OnTestProgramStart (test);
+    }
+
+    void quiet_event_listener::OnTestIterationStart (testing::UnitTest const & test,
+                                                     int iteration) {
+        listener_->OnTestIterationStart (test, iteration);
+    }
+
+    void quiet_event_listener::OnEnvironmentsSetUpStart (testing::UnitTest const &) {}
+    void quiet_event_listener::OnEnvironmentsSetUpEnd (testing::UnitTest const &) {}
+    void quiet_event_listener::OnEnvironmentsTearDownStart (testing::UnitTest const &) {}
+    void quiet_event_listener::OnEnvironmentsTearDownEnd (testing::UnitTest const &) {}
+
+    void quiet_event_listener::OnTestCaseStart (testing::TestCase const &) {}
+    void quiet_event_listener::OnTestCaseEnd (testing::TestCase const &) {}
+    void quiet_event_listener::OnTestStart (testing::TestInfo const &) {}
+
+    void quiet_event_listener::OnTestPartResult (testing::TestPartResult const & result) {
+        listener_->OnTestPartResult (result);
+    }
+
+    void quiet_event_listener::OnTestEnd (testing::TestInfo const & test_info) {
+        if (test_info.result ()->Failed ()) {
+            listener_->OnTestEnd (test_info);
+        }
+    }
+
+    void quiet_event_listener::OnTestIterationEnd (testing::UnitTest const & test, int iteration) {
+        listener_->OnTestIterationEnd (test, iteration);
+    }
+
+    void quiet_event_listener::OnTestProgramEnd (testing::UnitTest const & test) {
+        listener_->OnTestProgramEnd (test);
+    }
+
+} // end anonymous namespace
 
 
 int main (int argc, char ** argv) {
@@ -149,19 +162,25 @@ int main (int argc, char ** argv) {
         // no need for calling testing::InitGoogleTest() separately.
         testing::InitGoogleMock (&argc, argv);
 
-        // Remove the default listener
-        testing::TestEventListeners & listeners = testing::UnitTest::GetInstance ()->listeners ();
-        testing::TestEventListener * const default_printer =
-            listeners.Release (listeners.default_result_printer ());
+        // Unless the user enables "loud mode" by passing the appropriate switch, we silence much of
+        // google test/mock's output so that we only see detailed information about tests that fail.
+        if (!loud_mode_enabled (argc, argv)) {
+            // Remove the default listener
+            testing::TestEventListeners & listeners =
+                testing::UnitTest::GetInstance ()->listeners ();
+            testing::TestEventListener * const default_printer =
+                listeners.Release (listeners.default_result_printer ());
 
-        // Add our listener. By default everything is on (as when using the default listener)
-        // but here we turn everything off so we only see the 3 lines for the result (plus any failures at the end), like:
-        //
-        // [==========] Running 149 tests from 53 test cases.
-        // [==========] 149 tests from 53 test cases ran. (1 ms total)
-        // [  PASSED  ] 149 tests.
+            // Add our listener. By default everything is on (as when using the default listener)
+            // but here we turn everything off so we only see the 3 lines for the result (plus any
+            // failures at the end), like:
+            //
+            // [==========] Running 149 tests from 53 test cases.
+            // [==========] 149 tests from 53 test cases ran. (1 ms total)
+            // [  PASSED  ] 149 tests.
 
-        listeners.Append (new quiet_event_listener (default_printer));
+            listeners.Append (new quiet_event_listener (default_printer));
+        }
 
 #if defined(_WIN32)
         // Disable all of the possible ways Windows conspires to make automated
