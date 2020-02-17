15bd4dcac7cde7449e2c12aa91ef33a50a9a103f
rgal <rgal@users.noreply.github.com>
2020-02-17 12:40:44 +0000

    Enable check-pstore to run pstore unittests as well as build them (#53)
    * Enable check-pstore to run pstore unittests as well as build them
    
    This runs them through lit (as with LLVM) so this integrates into
    check-all and can produce xunit.xml data.

---
+++ unittests/lit.cfg.py
@@ -0,0 +1,70 @@
+# -*- Python -*-
+# *  _ _ _          __        *
+# * | (_) |_   ___ / _| __ _  *
+# * | | | __| / __| |_ / _` | *
+# * | | | |_ | (__|  _| (_| | *
+# * |_|_|\__(_)___|_|  \__, | *
+# *                    |___/  *
+# ===- unittests/lit.cfg.py -----------------------------------------------===//
+#  Copyright (c) 2017-2020 by Sony Interactive Entertainment, Inc.
+#  All rights reserved.
+#
+#  Developed by:
+#    Toolchain Team
+#    SN Systems, Ltd.
+#    www.snsystems.com
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a
+#  copy of this software and associated documentation files (the
+#  "Software"), to deal with the Software without restriction, including
+#  without limitation the rights to use, copy, modify, merge, publish,
+#  distribute, sublicense, and/or sell copies of the Software, and to
+#  permit persons to whom the Software is furnished to do so, subject to
+#  the following conditions:
+#
+#  - Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimers.
+#
+#  - Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimers in the
+#    documentation and/or other materials provided with the distribution.
+#
+#  - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
+#    Inc. nor the names of its contributors may be used to endorse or
+#    promote products derived from this Software without specific prior
+#    written permission.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+#  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+#  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+#  IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+#  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+#  TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+#  SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+# ===----------------------------------------------------------------------===//
+
+# Configuration file for the 'lit' test runner.
+
+import os
+import lit.formats
+
+# name: The name of this test suite.
+config.name = 'pstore-Unit'
+
+# suffixes: A list of file extensions to treat as test files.
+config.suffixes = []
+
+# test_source_root: The root path where tests are located.
+# test_exec_root: The root path where tests should be run.
+config.test_exec_root = os.path.join(config.pstore_obj_root, 'unittests')
+config.test_source_root = config.test_exec_root
+
+# testFormat: The test format to use to interpret tests.
+config.test_format = lit.formats.GoogleTest(config.llvm_build_mode, 'unit-tests')
+
+# Propagate the temp directory. Windows requires this because it uses \Windows\
+# if none of these are present.
+if 'TMP' in os.environ:
+    config.environment['TMP'] = os.environ['TMP']
+if 'TEMP' in os.environ:
+    config.environment['TEMP'] = os.environ['TEMP']
