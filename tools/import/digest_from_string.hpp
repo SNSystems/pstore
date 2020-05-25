fabbf77bbbfe55bcaf9ca747ac73d873801ad68e
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-05-25 09:35:53 +0100

    License boilerplate.

--- tools/import/digest_from_string.hpp
+++ tools/import/digest_from_string.hpp
@@ -1,12 +1,54 @@
+//*      _ _                 _      __                      *
+//*   __| (_) __ _  ___  ___| |_   / _|_ __ ___  _ __ ___   *
+//*  / _` | |/ _` |/ _ \/ __| __| | |_| '__/ _ \| '_ ` _ \  *
+//* | (_| | | (_| |  __/\__ \ |_  |  _| | | (_) | | | | | | *
+//*  \__,_|_|\__, |\___||___/\__| |_| |_|  \___/|_| |_| |_| *
+//*          |___/                                          *
+//*      _        _              *
+//*  ___| |_ _ __(_)_ __   __ _  *
+//* / __| __| '__| | '_ \ / _` | *
+//* \__ \ |_| |  | | | | | (_| | *
+//* |___/\__|_|  |_|_| |_|\__, | *
+//*                       |___/  *
+//===- tools/import/digest_from_string.hpp --------------------------------===//
+// Copyright (c) 2017-2020 by Sony Interactive Entertainment, Inc.
+// All rights reserved.
 //
-//  digest_from_string.hpp
-//  gmock
+// Developed by:
+//   Toolchain Team
+//   SN Systems, Ltd.
+//   www.snsystems.com
 //
-//  Created by Paul Bowen-Huggett on 22/05/2020.
+// Permission is hereby granted, free of charge, to any person obtaining a
+// copy of this software and associated documentation files (the
+// "Software"), to deal with the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
 //
-
-#ifndef digest_from_string_hpp
-#define digest_from_string_hpp
+// - Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimers.
+//
+// - Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimers in the
+//   documentation and/or other materials provided with the distribution.
+//
+// - Neither the names of SN Systems Ltd., Sony Interactive Entertainment,
+//   Inc. nor the names of its contributors may be used to endorse or
+//   promote products derived from this Software without specific prior
+//   written permission.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+// IN NO EVENT SHALL THE CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+// ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+// TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+// SOFTWARE OR THE USE OR OTHER DEALINGS WITH THE SOFTWARE.
+//===----------------------------------------------------------------------===//
+#ifndef PSTORE_IMPORT_DIGEST_FROM_STRING_HPP
+#define PSTORE_IMPORT_DIGEST_FROM_STRING_HPP
 
 #include <string>
 #include "pstore/support/maybe.hpp"
@@ -14,4 +56,4 @@
 
 pstore::maybe<pstore::uint128> digest_from_string (std::string const & str);
 
-#endif /* digest_from_string_hpp */
+#endif // PSTORE_IMPORT_DIGEST_FROM_STRING_HPP
