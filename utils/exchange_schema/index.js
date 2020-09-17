07a21465eeed97540230d4fd378192f06ae93e10
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-09-17 13:53:11 +0200

    Remove dependency on yargs.
    This requires Node.JS v10 or later and the Travis-CI machines have
    v8. Fortunately, the parsing requirement here is extremely simple so
    replacement is a trivial thing.

--- utils/exchange_schema/index.js
+++ utils/exchange_schema/index.js
@@ -7,14 +7,14 @@ const util = require ('util');
 
 // Convert `fs.readFile()` into a function that takes the
 // same parameters but returns a promise.
-const read_file = util.promisify(fs.readFile);
+const read_file = util.promisify (fs.readFile);
 
 async function read_json (path) {
     return JSON.parse (await read_file (path, 'utf-8'));
 }
 
 async function read_schema (schema_path) {
-    const ajv = new Ajv ({allErrors:true});
+    const ajv = new Ajv ({allErrors: true});
     return ajv.compile (await read_json (schema_path));
 }
 
@@ -22,22 +22,16 @@ async function read_schema_and_instance (schema_path, instance_path) {
     return Promise.all ([read_schema (schema_path), read_json (instance_path)]);
 }
 
-async function main () {
-    const argv = require ('yargs')
-        .strict ()
-        .command ('$0 schema instance', 'Validate JSON against a schema', (yargs) => {
-            yargs.positional('schema', {
-                describe: 'The path to the JSON schema file against which the instance will be checked.',
-                type: 'string',
-            });
-            yargs.positional('instance', {
-                describe: 'The path to the JSON instance file.',
-                type: 'string',
-            });
-        })
-        .argv;
-
-    const [schema, instance] = await read_schema_and_instance (argv.schema, argv.instance);
+async function main (argv) {
+    const args = argv.slice (2);
+    if (args.length !== 2) {
+        console.log (`${argv[1]} <schema> <instance>`);
+        console.log ('Positionals:');
+        console.log ('    schema    The path to the JSON schema file against which the instance will be checked.');
+        console.log ('    instance  The path to the JSON instance file.');
+        return;
+    }
+    const [schema, instance] = await read_schema_and_instance (args[0], args[1]);
     const result = schema (instance)
     if (!result) {
         throw new Error (JSON.stringify (schema.errors, null, 4));
@@ -45,8 +39,8 @@ async function main () {
 }
 
 (async () => {
-    await main();
-})().catch(e => {
+    await main (process.argv);
+}) ().catch (e => {
     console.error (e);
     process.exitCode = 1;
 });
