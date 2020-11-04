0dd1b4e3d4d7e6d64d124aef28faef9e677a4c73
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-11-04 10:54:54 +0100

    Document fields of linked_deifnitions::value_type.
    value_type::operator== doesn't need to be inlined. Misc comments.

--- lib/mcrepo/linked_definitions_section.cpp
+++ lib/mcrepo/linked_definitions_section.cpp
@@ -52,21 +52,35 @@
 namespace pstore {
     namespace repo {
 
+        //*           _            _                   *
+        //* __ ____ _| |_  _ ___  | |_ _  _ _ __  ___  *
+        //* \ V / _` | | || / -_) |  _| || | '_ \/ -_) *
+        //*  \_/\__,_|_|\_,_\___|  \__|\_, | .__/\___| *
+        //*                            |__/|_|         *
+
+        // operator==
+        // ~~~~~~~~~~
+        bool linked_definitions::value_type::operator== (value_type const & rhs) const noexcept {
+            if (&rhs == this) {
+                return true;
+            }
+            return compilation == rhs.compilation && index == rhs.index && pointer == rhs.pointer;
+        }
+
+
         //*  _ _      _          _      _      __ _      _ _   _              *
         //* | (_)_ _ | |_____ __| |  __| |___ / _(_)_ _ (_) |_(_)___ _ _  ___ *
         //* | | | ' \| / / -_) _` | / _` / -_)  _| | ' \| |  _| / _ \ ' \(_-< *
         //* |_|_|_||_|_\_\___\__,_| \__,_\___|_| |_|_||_|_|\__|_\___/_||_/__/ *
         //*                                                                   *
-        // size_bytes
+
+        // size bytes
         // ~~~~~~~~~~
         std::size_t linked_definitions::size_bytes (std::uint64_t const size) noexcept {
             return sizeof (linked_definitions) - sizeof (definitions_) +
                    sizeof (definitions_[0]) * size;
         }
 
-        // size_bytes
-        // ~~~~~~~~~~
-        /// Returns the number of bytes of storage required for the linked definitions.
         std::size_t linked_definitions::size_bytes () const noexcept {
             return size_bytes (this->size ());
         }
@@ -92,6 +106,9 @@ namespace pstore {
         //* / _| '_/ -_) _` |  _| / _ \ ' \  / _` | (_-< '_ \/ _` |  _/ _| ' \/ -_) '_| *
         //* \__|_| \___\__,_|\__|_\___/_||_| \__,_|_/__/ .__/\__,_|\__\__|_||_\___|_|   *
         //*                                            |_|                              *
+
+        // size bytes
+        // ~~~~~~~~~~
         std::size_t linked_definitions_creation_dispatcher::size_bytes () const {
             static_assert (sizeof (std::uint64_t) >= sizeof (std::uintptr_t),
                            "sizeof uint64_t should be at least sizeof uintptr_t");
@@ -100,6 +117,8 @@ namespace pstore {
                 static_cast<std::uint64_t> (std::distance (begin_, end_)));
         }
 
+        // write
+        // ~~~~~
         std::uint8_t *
         linked_definitions_creation_dispatcher::write (std::uint8_t * const out) const {
             assert (this->aligned (out) == out);
@@ -107,6 +126,8 @@ namespace pstore {
             return out + dependent->size_bytes ();
         }
 
+        // aligned impl
+        // ~~~~~~~~~~~~
         std::uintptr_t
         linked_definitions_creation_dispatcher::aligned_impl (std::uintptr_t const in) const {
             return pstore::aligned<linked_definitions> (in);
