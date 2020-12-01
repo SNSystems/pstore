33cf0b044c597b89b6b1eecf5b57e63ff921ec59
Paul Bowen-Huggett <paulhuggett@mac.com>
2020-12-01 17:21:33 +0100

    Refactor to write to an output-iterator rather than a deque<>.
    This enables the called to avoid creating a temporary container where
    none is necessary. The original API is retained for compatibility but
    the result_type now uses chunked_vector<> rather than deque<>.

--- include/pstore/diff/diff.hpp
+++ include/pstore/diff/diff.hpp
@@ -44,15 +44,11 @@
 #ifndef PSTORE_DIFF_DIFF_HPP
 #define PSTORE_DIFF_DIFF_HPP
 
-#include <deque>
-
 #include "pstore/core/hamt_set.hpp"
 #include "pstore/diff/revision.hpp"
 
 namespace pstore {
     namespace diff {
-
-        using result_type = std::deque<address>;
 
         namespace details {
 
@@ -64,27 +60,39 @@ namespace pstore {
                 /// \param db  The owning database instance.
                 /// \param index  The index to be traversed.
                 /// \param threshold  Addresses less than the threshold value are "old".
-                traverser (database const & db, Index const & index, address threshold) noexcept
+                constexpr traverser (database const & db, Index const & index,
+                                     address threshold) noexcept
                         : db_{db}
                         , index_{index}
-                        , threshold_ (std::move (threshold)) {}
+                        , threshold_{threshold} {}
 
-                result_type operator() () const;
+                /// \tparam OutputIterator An Output-Iterator type.
+                /// \param out  The output iterator to which the address of leaves may be added.
+                /// \result The output iterator to which results were written.
+                template <typename OutputIterator>
+                OutputIterator operator() (OutputIterator out) const;
 
             private:
+                /// \tparam OutputIterator An Output-Iterator type.
                 /// \param node  The index node to be visited.
                 /// \param shifts  The depth of the node in the tree structure.
-                /// \param result  A container to which the address of leaf nodes may be added.
-                void visit_node (index_pointer node, unsigned shifts,
-                                 gsl::not_null<result_type *> result) const;
+                /// \param out  The output iterator to which the address of leaves may be added.
+                /// \result The output iterator to which results were written.
+                template <typename OutputIterator>
+                OutputIterator visit_node (index_pointer node, unsigned shifts,
+                                           OutputIterator out) const;
 
                 /// Recursively traverses the members of an internal or linear index node.
                 ///
+                /// \tparam Node An index node type: either internal_node or linear_node.
+                /// \tparam OutputIterator An Output-Iterator type.
+                /// \param node  The index node to be visited.
                 /// \param shifts  The depth of the node in the tree structure.
-                /// \param result  A container to which the address of leaf nodes may be added.
-                template <typename Node>
-                void visit_intermediate (index_pointer node, unsigned shifts,
-                                         gsl::not_null<result_type *> result) const;
+                /// \param out  The output iterator to which the address of leaves may be added.
+                /// \result The output iterator to which results were written.
+                template <typename Node, typename OutputIterator>
+                OutputIterator visit_intermediate (index_pointer node, unsigned shifts,
+                                                   OutputIterator out) const;
 
                 bool is_new (index_pointer const node) const noexcept {
                     return node.is_heap () ||
@@ -99,64 +107,97 @@ namespace pstore {
             // operator()
             // ~~~~~~~~~~
             template <typename Index>
-            result_type traverser<Index>::operator() () const {
-                result_type result;
+            template <typename OutputIterator>
+            OutputIterator traverser<Index>::operator() (OutputIterator out) const {
                 if (auto const root = index_.root ()) {
-                    this->visit_node (root, 0U, &result);
+                    out = this->visit_node (root, 0U, out);
                 }
-                return result;
+                return out;
             }
 
-            // visit_node
+            // visit node
             // ~~~~~~~~~~
             template <typename Index>
-            void traverser<Index>::visit_node (index_pointer node, unsigned shifts,
-                                               gsl::not_null<result_type *> result) const {
+            template <typename OutputIterator>
+            OutputIterator traverser<Index>::visit_node (index_pointer node, unsigned shifts,
+                                                         OutputIterator out) const {
                 if (node.is_leaf ()) {
                     assert (node.is_address ());
                     // If this leaf is not in the "old" byte range then add it to the output
                     // collection.
                     if (this->is_new (node)) {
-                        result->push_back (node.addr);
+                        *out = node.addr;
+                        ++out;
                     }
-                } else if (index::details::depth_is_internal_node (shifts)) {
-                    this->visit_intermediate<index::details::internal_node> (node, shifts, result);
-                } else {
-                    this->visit_intermediate<index::details::linear_node> (node, shifts, result);
+                    return out;
                 }
+
+                if (index::details::depth_is_internal_node (shifts)) {
+                    return this->visit_intermediate<index::details::internal_node> (node, shifts,
+                                                                                    out);
+                }
+
+                return this->visit_intermediate<index::details::linear_node> (node, shifts, out);
             }
 
-            // visit_intermediate
+            // visit intermediate
             // ~~~~~~~~~~~~~~~~~~
             template <typename Index>
-            template <typename Node>
-            void
+            template <typename Node, typename OutputIterator>
+            OutputIterator
             traverser<Index>::visit_intermediate (index::details::index_pointer const node,
-                                                  unsigned const shifts,
-                                                  gsl::not_null<result_type *> const result) const {
+                                                  unsigned const shifts, OutputIterator out) const {
                 std::pair<std::shared_ptr<void const>, Node const *> const p =
                     Node::get_node (db_, node);
-                assert (std::get<1> (p) != nullptr);
-                for (auto child : *std::get<1> (p)) {
+                assert (std::get<Node const *> (p) != nullptr);
+                for (auto child : *std::get<Node const *> (p)) {
                     if (this->is_new (node)) {
-                        this->visit_node (index_pointer{child},
-                                          shifts + index::details::hash_index_bits, result);
+                        out = this->visit_node (index_pointer{child},
+                                                shifts + index::details::hash_index_bits, out);
                     }
                 }
+                return out;
             }
 
         } // end namespace details
 
 
-        template <typename Index>
-        result_type diff (database const & db, Index const & index, revision_number const old) {
+        /// Write a series of addresses to an output iterator of the objects that were added to an
+        /// \p index between the current revision and the revision number given by \p old.
+        ///
+        /// \param db  The owning database instance.
+        /// \param index  The index to be traversed.
+        /// \param old  The revision number against which the index is to be compared.
+        /// \param out  The output iterator to which the address of objects added to the index since
+        ///   the given old revision.
+        /// \result The output iterator to which results were written.
+        template <typename Index, typename OutputIterator>
+        OutputIterator diff (database const & db, Index const & index, revision_number const old,
+                             OutputIterator out) {
             if (old == pstore::head_revision || old > db.get_current_revision ()) {
-                return {};
+                return out;
             }
             // addresses less than the threshold value are "old".
             details::traverser<Index> t{db, index,
                                         (db.older_revision_footer_pos (old) + 1).to_address ()};
-            return t ();
+            return t (out);
+        }
+
+        using result_type = chunked_vector<address>;
+
+        /// Produces a collection of address which are those objects that were added to an \p index
+        /// between the current revision and the revision number given by \p old.
+        ///
+        /// \param db  The owning database instance.
+        /// \param index  The index to be traversed.
+        /// \param old  The revision number against which the index is to be compared.
+        /// \result A collection of address which were added to the index since the given old
+        ///    revision.
+        template <typename Index>
+        result_type diff (database const & db, Index const & index, revision_number const old) {
+            result_type result;
+            diff (db, index, old, std::back_inserter (result));
+            return result;
         }
 
     } // end namespace diff
