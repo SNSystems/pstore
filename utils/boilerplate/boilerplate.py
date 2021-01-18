159ce7a283df1b5af05f01629d339f8096c5f057
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-01-18 12:39:46 +0100

    Correct the license file link.

--- utils/boilerplate/boilerplate.py
+++ utils/boilerplate/boilerplate.py
@@ -59,7 +59,7 @@ import sys
 license_text = \
 '''
 Part of the pstore project, under the Apache License v2.0 with LLVM Exceptions.
-See https://llvm.org/LICENSE.txt for license information.
+See https://github.com/SNSystems/pstore/blob/master/LICENSE.txt for license information.
 SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 '''
 
