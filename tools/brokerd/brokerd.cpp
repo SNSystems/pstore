dc2309c40f9ff9cd8883709f4e2fc3ceb7add28f
Nabeel Omer <Nabeel.Omer@sony.com>
2021-05-19 17:57:44 +0100

    Add banners

--- tools/brokerd/brokerd.cpp
+++ tools/brokerd/brokerd.cpp
@@ -1,3 +1,20 @@
+//===- tools/brokerd/brokerd.cpp ---------------------------------------------===//
+//*  _               _                _  *
+//* | |             | |              | | *
+//* | |__  _ __ ___ | | _____ _ __ __| | *
+//* | '_ \| '__/ _ \| |/ / _ \ '__/ _` | *
+//* | |_) | | | (_) |   <  __/ | | (_| | *
+//* |_.__/|_|  \___/|_|\_\___|_|  \__,_| *
+//*                                      *
+//===----------------------------------------------------------------------===//
+//
+// Part of the pstore project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://github.com/SNSystems/pstore/blob/master/LICENSE.txt for license
+// information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 // Standard includes
 #include <cstring>
 #include <fstream>
