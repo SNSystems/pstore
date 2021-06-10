8b79a3c512c9ca81c853528acfccc6391d22ae50
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-06-10 09:39:04 +0200

    Omit the debug-line-header property is not needed.
    If a transaction does not contain any new debug-line-header entries
    then there is no need to emit the corresponding property in the
    transaction object.

--- lib/exchange/export.cpp
+++ lib/exchange/export.cpp
@@ -24,6 +24,8 @@
 
 namespace {
 
+    // footers
+    // ~~~~~~~
     decltype (auto) footers (pstore::database const & db) {
         auto const num_transactions = db.get_current_revision () + 1;
         std::vector<pstore::typed_address<pstore::trailer>> footers;
@@ -37,38 +39,43 @@ namespace {
         return footers;
     }
 
-    //*            _ _        _     _                _ _           *
-    //*  ___ _ __ (_) |_   __| |___| |__ _  _ __ _  | (_)_ _  ___  *
-    //* / -_) '  \| |  _| / _` / -_) '_ \ || / _` | | | | ' \/ -_) *
-    //* \___|_|_|_|_|\__| \__,_\___|_.__/\_,_\__, | |_|_|_||_\___| *
-    //*                                      |___/                 *
-
-    void emit_debug_line_headers (pstore::exchange::export_ns::ostream & os,
+    // emit debug line headers
+    // ~~~~~~~~~~~~~~~~~~~~~~~
+    bool emit_debug_line_headers (pstore::exchange::export_ns::ostream & os,
                                   pstore::exchange::export_ns::indent const ind,
                                   pstore::database const & db, unsigned const generation) {
         auto const debug_line_headers =
             pstore::index::get_index<pstore::trailer::indices::debug_line_header> (db);
-        if (!debug_line_headers->empty ()) {
-            auto const * sep = "\n";
-            PSTORE_ASSERT (generation > 0);
-
-            auto const out_fn = [&] (pstore::address const addr) {
-                auto const & kvp = debug_line_headers->load_leaf_node (db, addr);
-                os << sep << ind;
-                pstore::exchange::export_ns::emit_digest (os, kvp.first);
-                os << ':';
-                std::shared_ptr<std::uint8_t const> const data = db.getro (kvp.second);
-                auto const * const ptr = data.get ();
-                pstore::to_base64 (ptr, ptr + kvp.second.size,
-                                   pstore::exchange::export_ns::ostream_inserter{os});
-                os << '"';
-
-                sep = ",\n";
-            };
-
-            pstore::diff (db, *debug_line_headers, generation - 1U,
-                                pstore::exchange::export_ns::make_diff_out (&out_fn));
+        if (debug_line_headers->empty ()) {
+            return false;
         }
+
+        PSTORE_ASSERT (generation > 0);
+        bool first = true;
+        auto const object_indent = ind.next ();
+        auto const out_fn = [&] (pstore::address const addr) {
+            if (first) {
+                os << ind << R"("debugline":{)";
+                first = false;
+            } else {
+                os << ',';
+            }
+            os << '\n' << object_indent;
+            auto const & kvp = debug_line_headers->load_leaf_node (db, addr);
+            pstore::exchange::export_ns::emit_digest (os, kvp.first);
+            os << R"(:")";
+            std::shared_ptr<std::uint8_t const> const data = db.getro (kvp.second);
+            auto const * const ptr = data.get ();
+            pstore::to_base64 (ptr, ptr + kvp.second.size,
+                               pstore::exchange::export_ns::ostream_inserter{os});
+            os << '"';
+        };
+        pstore::diff (db, *debug_line_headers, generation - 1U,
+                      pstore::exchange::export_ns::make_diff_out (&out_fn));
+        if (!first) {
+            os << '\n' << ind << "},\n";
+        }
+        return first;
     }
 
 } // end anonymous namespace
@@ -107,9 +114,10 @@ namespace pstore {
                         os1 << ",\n" << object_indent << R"("paths":)";
                         emit_strings<trailer::indices::path> (os1, object_indent, db, generation,
                                                               &path_table);
-                        os1 << ",\n" << object_indent << R"("debugline":{)";
-                        emit_debug_line_headers (os1, object_indent.next (), db, generation);
-                        os1 << '\n' << object_indent << "},\n";
+                        os1 << ",\n";
+                        if (emit_debug_line_headers (os1, object_indent, db, generation)) {
+                            os1 << ",\n";
+                        }
                         os1 << object_indent << R"("fragments":{)";
                         emit_fragments (os1, object_indent.next (), db, generation, string_table,
                                         comments);
