76826198db005172a9fd5f40877650a320279cc2
Paul Bowen-Huggett <paulhuggett@mac.com>
2021-09-17 11:42:30 +0200

    Remove unused import.

--- tools/broker_ui/preload.js
+++ tools/broker_ui/preload.js
@@ -13,7 +13,7 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-const {contextBridge, ipcRenderer, nativeTheme} = require ('electron')
+const {contextBridge, ipcRenderer} = require ('electron')
 
 contextBridge.exposeInMainWorld('bridgeAPI', {
     ipcRenderer : () => {
