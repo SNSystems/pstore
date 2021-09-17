SNSystems/pstore

commit 976cca593c6af7fed158651b7fef236583e8aba8

Update to electron 12.

tools/broker_ui/main.js
@@ -13,48 +13,50 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-const {app, BrowserWindow, nativeTheme} = require ('electron');
-
+const {app, BrowserWindow, ipcMain, nativeTheme} = require ('electron');
+const path = require ('path')
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let main_window;
+let mainWindow;
 
-function dark_side () {
-    main_window.webContents.send('dark-mode', nativeTheme.shouldUseDarkColors);
+function darkSide () {
+    mainWindow.webContents.send('dark-mode', nativeTheme.shouldUseDarkColors);
 }
-nativeTheme.on('updated', dark_side);
 
 function createWindow () {
     // Create the browser window.
-
-    main_window = new BrowserWindow ({
+    mainWindow = new BrowserWindow ({
         width : 800,
         height : 600,
         show : false,
-        webPreferences : {
-            nodeIntegration : true,
-            enableRemoteModule: true
-        },
+        webPreferences : {preload : path.join(__dirname, 'preload.js')},
     });
-    main_window.once('ready-to-show', () => {
-        dark_side ();
-        main_window.show();
+    mainWindow.once('ready-to-show', () => {
+        darkSide ();
+        mainWindow.show();
     });
 
     // and load the index.html of the app.
-    main_window.loadFile('index.html');
+    mainWindow.loadFile('index.html');
 
     // Open the DevTools.
-    //main_window.webContents.openDevTools();
+    main_window.webContents.openDevTools();
 
     // Emitted when the window is closed.
-    main_window.on('closed', function () {
+    mainWindow.on('closed', function () {
         // Dereference the window object, usually you would store windows
         // in an array if your app supports multi windows, this is the time
         // when you should delete the corresponding element.
-        main_window = null;
+        mainWindow = null;
     });
+
+
+
+    //ipcMain.handle('dark-mode:system', () => {nativeTheme.themeSource = 'system'})
+    return mainWindow;
 }
+
+
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -73,7 +75,7 @@ app.on('window-all-closed', function () {
 app.on('activate', function () {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
-    if (main_window === null) {
+    if (BrowserWindow.getAllWindows().length === 0) {
         createWindow ()
     }
 });

tools/broker_ui/renderer.js
@@ -13,13 +13,12 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-const {ipcRenderer} = require ('electron');
-const {dialog} = require ('electron').remote;
-let $ = require ('jquery');
-const {series} = require('./series');
+import {series} from './series.js'
+import * as ds from './node_modules/d3/dist/d3.min.js'
+const bridgeAPI = window.bridgeAPI;
 
-function new_ws (host, channel, on_message) {
-    const socket = new WebSocket ('ws://' + host + '/' + channel);
+function newWS (port, channel, on_message) {
+    const socket = new WebSocket ('ws://localhost:' + port + '/' + channel);
     socket.onerror = event => {
         dialog.showMessageBox({
             type: "error",
@@ -31,7 +30,7 @@ function new_ws (host, channel, on_message) {
         }).then (p => {
             console.log ('button clicked was ', p.response);
             if (p.response === 1) {
-                return new_ws (host, channel, on_message);
+                return newWS (host, channel, on_message);
             }
         }).catch (error => {
             console.error (error);
@@ -43,35 +42,32 @@ function new_ws (host, channel, on_message) {
     return socket;
 }
 
-let prev_commits = 0;
+let prevCommits = 0;
 let commits = 0;
 
-series ((time) => {
-    const arrived = commits - prev_commits;
-    $('#tps').text(arrived);
-    prev_commits = commits;
+series (time => {
+    const arrived = commits - prevCommits;
+    d3.select('#tps').text(arrived);
+    prevCommits = commits;
     return arrived;
 });
 
 // There is a single for processing channel messages because I want to move to a model where there is a single
 // WebSocket for all the channels. A JSON object is sent requesting subscription and unsubscription and each
 // channel gets a member of the received message object.
-function message_received (msg) {
+function messageReceived (msg) {
     const obj = JSON.parse(msg.data);
     if (obj.hasOwnProperty('uptime')) {
-        $('#uptime').text (moment.duration(obj.uptime, 'seconds').humanize());
+        d3.select('#uptime').text(humanizeDuration(obj.uptime * 1000));
     }
     if (obj.hasOwnProperty('commits')) {
-        commits = obj.commits;
-        $('#commits').text (obj.commits);
+        d3.select('#commits').text(obj.commits);
     }
 }
 
-const localhost = 'localhost:8080';
+const port = 8080
 //const localhost = window.location.host;
-new_ws (localhost,'uptime', message_received);
-new_ws (localhost,'commits', message_received);
+newWS (port, 'uptime', messageReceived);
+newWS (port, 'commits', messageReceived);
 
-ipcRenderer.on('dark-mode', (event, message) => {
-    document.documentElement.setAttribute('data-theme', message ? 'dark' : 'light');
-});
+bridgeAPI.ipcRenderer();

tools/broker_ui/index.html
@@ -3,6 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="Content-Security-Policy" content="script-src 'self'">
 
     <title>pstore Status</title>
     <link rel="stylesheet" href="./node_modules/bootstrap/dist/css/bootstrap.min.css">
@@ -24,7 +25,7 @@
         <div class="col">
           <div>
             <div style="display:inline;" class="ui-text">Server uptime:</div>
-            <div style="display:inline;" class="ui-text" id="uptime">0</div>
+            <div style="display:inline;" class="ui-text" id="uptime">unknown</div>
           </div>
 
           <div>
@@ -46,15 +47,13 @@
 
     <!-- Bootstrap Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script>let $ = require('jquery');</script>
+<!--    <script>let $ = require('jquery');</script>
     <script>let moment = require('moment');</script>
 
     <script>require('popper.js');</script>
     <script>require('bootstrap');</script>
-
-    <script>
-      // You can also require other files to run in this process
-      var r = require('./renderer.js');
-    </script>
+-->
+    <script src="./node_modules/humanize-duration/humanize-duration.js"></script>
+    <script type="module" src="./renderer.js"></script>
   </body>
 </html>

tools/broker_ui/series.js
@@ -13,10 +13,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 //
 //===----------------------------------------------------------------------===//
-let d3 = require ('d3');
+import * as ds from './node_modules/d3/dist/d3.min.js'
 
-exports.series = function series (pull) {
-    const n = 20; // The number of data points shown.
+export function series (pull) {
+    const n = 20 // The number of data points shown.
     const timeFormat = '%H:%M:%S';
     const margin = {
         top: 20,
@@ -28,9 +28,7 @@ exports.series = function series (pull) {
     const curve = d3.curveBasis; // d3.curveLinear
     const offset = 2; // set to 2 if curve===curveBasis, 1 if curveLinear
 
-    const xDomain = (t) => {
-        return [t - ((n - offset) * duration), t - (offset * duration)];
-    };
+    const xDomain = t => [t - ((n - offset) * duration), t - (offset * duration)];
 
     const data = [];
 
@@ -46,22 +44,19 @@ exports.series = function series (pull) {
         .domain ([0, 1])
         .range ([height, 0]);
 
-    const line = d3.line ()
-        .x ((d) => { return x (d.time); })
-        .y ((d) => { return y (d.value); })
-        .curve (curve);
+    const line = d3.line().x(d => x (d.time)).y(d => y (d.value)).curve(curve);
 
     const xAxisCall = d3.axisBottom (x).tickFormat (d3.timeFormat (timeFormat));
     const yAxisCall = d3.axisLeft (y);
 
     const g = svg.append ('g')
         .attr ('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-    g.append ('defs')
-        .append ('clipPath')
-        .attr ('id', 'clip')
-        .append ('rect')
-        .attr ('width', width)
-        .attr ('height', height);
+    g.append('defs')
+        .append('clipPath')
+        .attr('id', 'clip')
+        .append('rect')
+        .attr('width', width)
+        .attr('height', height);
 
     const xAxis = g.append ('g')
         .attr ('class', 'axis axis-x')
@@ -91,9 +86,7 @@ exports.series = function series (pull) {
         }
 
         x.domain (xDomain (time));
-        y.domain ([0, Math.max (1.0, d3.max (data, (d) => {
-            return d.value;
-        }))]);
+        y.domain([ 0, Math.max(1.0, d3.max(data, d => d.value)) ]);
 
         // Redraw the line.
         path.attr ('d', line).attr ('transform', null);

commit 021a2f4bd82568b1de345578af94d867707af3fe

Set the commit global when a commit message is received.
This was broken by commit 976cca5.

tools/broker_ui/main.js
@@ -40,7 +40,7 @@ function createWindow () {
     mainWindow.loadFile('index.html');
 
     // Open the DevTools.
-    main_window.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
 
     // Emitted when the window is closed.
     mainWindow.on('closed', function () {
@@ -49,8 +49,6 @@ function createWindow () {
         // when you should delete the corresponding element.
         mainWindow = null;
     });
-
-
 
     //ipcMain.handle('dark-mode:system', () => {nativeTheme.themeSource = 'system'})
     return mainWindow;

tools/broker_ui/renderer.js
@@ -61,7 +61,8 @@ function messageReceived (msg) {
         d3.select('#uptime').text(humanizeDuration(obj.uptime * 1000));
     }
     if (obj.hasOwnProperty('commits')) {
-        d3.select('#commits').text(obj.commits);
+        commits = obj.commits;
+        d3.select('#commits').text(commits);
     }
 }
 